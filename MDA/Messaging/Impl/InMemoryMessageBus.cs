b071e85ba352ffe68a4b03a8e6e66b7331675856
justmine <justmine@DESKTOP-UILU6SC>
2018-08-22 07:39:02 -0700

    重构消息总线

--- MDA/Messaging/Impl/InMemoryMessageBus.cs
+++ MDA/Messaging/Impl/InMemoryMessageBus.cs
@@ -1,6 +1,7 @@
 ﻿using MDA.Common;
-using System;
+using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace MDA.Messaging.Impl
@@ -10,51 +11,100 @@ namespace MDA.Messaging.Impl
     /// </summary>
     public class InMemoryMessageBus : IMessageBus
     {
-        public InMemoryMessageBus() 
-            : this("InMemoryMessageBus")
-        {
+        private readonly ILogger<InMemoryMessageBus> _logger;
+        private readonly IMessageSubscriberManager _subscriberManager;
 
+        public InMemoryMessageBus(
+            IMessageSubscriberManager subscriberManager,
+            ILogger<InMemoryMessageBus> logger)
+        {
+            _logger = logger;
+            _subscriberManager = subscriberManager;
         }
 
-        public InMemoryMessageBus(string name)
+        public string Name { get; } = "InMemoryMessageBus";
+
+        public async Task HandleAsync(IMessage message)
         {
-            Name = name;
+            await PublishAsync(message);
         }
 
-        public string Name { get; private set; }
-
-        public Task<AsyncResult> PublishAllAsync<TMessage>(IEnumerable<TMessage> messages) where TMessage : IMessage
+        public Task HandleAsync(dynamic message)
         {
-            throw new NotImplementedException();
+            throw new System.NotImplementedException();
         }
 
-        public Task<AsyncResult> PublishAsync<TMessage>(TMessage message) where TMessage : IMessage
+        public async Task<AsyncResult> PublishAllAsync<TMessage>(IEnumerable<TMessage> messages)
+            where TMessage : IMessage
         {
-            throw new NotImplementedException();
+            Assert.NotNull(messages, nameof(messages));
+
+            foreach (var message in messages)
+            {
+                var messageName = _subscriberManager.GetMessageName<TMessage>();
+                await DoPublishAsync(messageName, message);
+            }
+
+            return await Task.FromResult(AsyncResult.Success);
+        }
+
+        public async Task<AsyncResult> PublishAsync<TMessage>(TMessage message)
+            where TMessage : IMessage
+        {
+            Assert.NotNull<IMessage>(message, nameof(message));
+
+            var messageName = _subscriberManager.GetMessageName<TMessage>();
+            await DoPublishAsync(messageName, message);
+
+            return await Task.FromResult(AsyncResult.Success);
         }
 
         public void Subscribe<TMessage, TMessageHandler>()
             where TMessage : IMessage
             where TMessageHandler : IMessageHandler
         {
-            throw new NotImplementedException();
+            _subscriberManager.Subscribe<TMessage, TMessageHandler>();
         }
 
-        public void Subscribe<TMessageHandler>(string messageName) where TMessageHandler : IDynamicMessageHandler
+        public void SubscribeDynamic<TMessageHandler>(string messageName) where TMessageHandler : IDynamicMessageHandler
         {
-            throw new NotImplementedException();
+            _subscriberManager.SubscribeDynamic<TMessageHandler>(messageName);
         }
 
         public void Unsubscribe<TMessage, TMessageHandler>()
             where TMessage : IMessage
             where TMessageHandler : IMessageHandler
         {
-            throw new NotImplementedException();
+            _subscriberManager.Unsubscribe<TMessage, TMessageHandler>();
         }
 
-        public void UnsubscribeDynamic<TMessageHandler>(string eventName) where TMessageHandler : IDynamicMessageHandler
+        public void UnsubscribeDynamic<TMessageHandler>(string messageName) where TMessageHandler : IDynamicMessageHandler
         {
-            throw new NotImplementedException();
+            _subscriberManager.UnsubscribeDynamic<TMessageHandler>(messageName);
+        }
+
+        private async Task DoPublishAsync(string messageName, IMessage message)
+        {
+            Assert.NotNull(message, nameof(message));
+
+            var subscribers = _subscriberManager.GetHandlersForMessage(messageName);
+
+            if (subscribers.Any())
+            {
+                foreach (var subcriber in subscribers)
+                {
+                    if (subcriber.IsDynamic)
+                    {
+                        var handler = null as IDynamicMessageHandler;
+                        await handler.HandleAsync(message);
+                    }
+                    else
+                    {
+                        var handler = null as IMessageHandler;
+                        await handler.HandleAsync(message);
+                    }
+                }
+            }
         }
     }
 }
