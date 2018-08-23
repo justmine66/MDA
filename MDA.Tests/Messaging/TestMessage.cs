4108720725385b6ee8c07b881755b122f8a19af4
justmine <justmine@DESKTOP-UILU6SC>
2018-08-23 06:46:47 -0700

    添加测试类

---
+++ MDA.Tests/Messaging/TestMessage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using MDA.Messaging;
+
+namespace MDA.Tests.Messaging
+{
+    public class TestMessage : IMessage
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public DateTime Timestamp { get; set; } = DateTime.Now;
+    }
+}
---
+++ MDA.Tests/Messaging/TestMessageHandler.cs
@@ -0,0 +1,16 @@
+﻿using MDA.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDA.Tests.Messaging
+{
+    public class TestMessageHandler : IMessageHandler<TestMessage>
+    {
+        public Task HandleAsync(TestMessage message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
