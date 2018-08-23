4e35ddb71abcd62f05c415b087346acd1542f543
justmine <justmine@DESKTOP-UILU6SC>
2018-08-23 06:17:02 -0700

    添加MessagingOptions

---
+++ MDA/Messaging/MessagingOptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MDA.Messaging
+{
+    public class MessagingOptions
+    {
+        /// <summary>
+        /// 是否观察慢消息。
+        /// </summary>
+        /// <remarks>
+        /// 记录消息处理的时间，根据参数 <see cref="SlowMsgThreshold"/> 来衡量消息处理的快慢。
+        /// </remarks>
+        public bool WatchSlowMessage { get; set; }
+        /// <summary>
+        /// 慢消息阈值。
+        /// </summary>
+        public TimeSpan SlowMsgThreshold { get; set; }
+    }
+}
