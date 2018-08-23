ce9eac93f4990ef1758f3eb8974f7ac1efe119b5
justmine <justmine@DESKTOP-UILU6SC>
2018-08-23 06:24:11 -0700

    第一次添加MDS测试项目

---
+++ MDA.Tests/InMemory_Message_Subscriber_Manager_test.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+
+namespace MDA.Tests
+{
+    /// <summary>
+    /// 内存订阅管理器测试类
+    /// </summary>
+    public class InMemory_Message_Subscriber_Manager_Test
+    {
+        [Fact]
+        public void After_Creation_Should_Be_Empty()
+        {
+
+        }
+    }
+}
