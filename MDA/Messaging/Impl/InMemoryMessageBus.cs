73def03e8b12a637ee796f1a8d55b7501c8b8c39
justmine <justmine@DESKTOP-UILU6SC>
2018-08-23 06:14:33 -0700

    完成InMemoryMessageBus发布功能。

--- MDA/Messaging/Impl/InMemoryMessageBus.cs
+++ MDA/Messaging/Impl/InMemoryMessageBus.cs
@@ -1,5 +1,6 @@
 ﻿using MDA.Common;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,28 +14,24 @@ namespace MDA.Messaging.Impl
     {
         private readonly ILogger<InMemoryMessageBus> _logger;
         private readonly IMessageSubscriberManager _subscriberManager;
+        private readonly IServiceProvider _serviceProvider;
+        private readonly MessagingOptions _options;
 
         public InMemoryMessageBus(
             IMessageSubscriberManager subscriberManager,
-            ILogger<InMemoryMessageBus> logger)
+            ILogger<InMemoryMessageBus> logger,
+            IServiceProvider serviceProvider,
+            MessagingOptions options)
         {
             _logger = logger;
             _subscriberManager = subscriberManager;
+            _serviceProvider = serviceProvider;
+            _options = options;
         }
 
         public string Name { get; } = "InMemoryMessageBus";
 
-        public async Task HandleAsync(IMessage message)
-        {
-            await PublishAsync(message);
-        }
-
-        public Task HandleAsync(dynamic message)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public async Task<AsyncResult> PublishAllAsync<TMessage>(IEnumerable<TMessage> messages)
+        public async Task PublishAllAsync<TMessage>(IEnumerable<TMessage> messages)
             where TMessage : IMessage
         {
             Assert.NotNull(messages, nameof(messages));
@@ -44,19 +41,29 @@ namespace MDA.Messaging.Impl
                 var messageName = _subscriberManager.GetMessageName<TMessage>();
                 await DoPublishAsync(messageName, message);
             }
-
-            return await Task.FromResult(AsyncResult.Success);
         }
 
-        public async Task<AsyncResult> PublishAsync<TMessage>(TMessage message)
+        public async Task PublishAllDynamicAsync(string messageName, IEnumerable<dynamic> messages)
+        {
+            Assert.NotNullOrEmpty(messageName, nameof(messageName));
+            Assert.NotNull(messages, nameof(messages));
+
+            foreach (var message in messages)
+            {
+                await DoPublishAsync(messageName, message);
+            }
+        }
+
+        public async Task PublishAsync<TMessage>(TMessage message)
             where TMessage : IMessage
         {
-            Assert.NotNull<IMessage>(message, nameof(message));
-
             var messageName = _subscriberManager.GetMessageName<TMessage>();
             await DoPublishAsync(messageName, message);
+        }
 
-            return await Task.FromResult(AsyncResult.Success);
+        public async Task PublishDynamicAsync(string messageName, dynamic message)
+        {
+            await DoPublishAsync(messageName, message);
         }
 
         public void Subscribe<TMessage, TMessageHandler>()
@@ -85,23 +92,49 @@ namespace MDA.Messaging.Impl
 
         private async Task DoPublishAsync(string messageName, IMessage message)
         {
+            Assert.NotNullOrEmpty(messageName, nameof(messageName));
             Assert.NotNull(message, nameof(message));
 
-            var subscribers = _subscriberManager.GetHandlersForMessage(messageName);
+            var subscribers = _subscriberManager.GetSubscribers(messageName);
 
             if (subscribers.Any())
             {
                 foreach (var subcriber in subscribers)
                 {
+                    dynamic handler;
                     if (subcriber.IsDynamic)
                     {
-                        var handler = null as IDynamicMessageHandler;
-                        await handler.HandleAsync(message);
+                        handler = _serviceProvider.GetService(subcriber.HandlerType) as IDynamicMessageHandler;
                     }
                     else
                     {
-                        var handler = null as IMessageHandler;
-                        await handler.HandleAsync(message);
+                        handler = _serviceProvider.GetService(subcriber.HandlerType) as IMessageHandler;
+                    }
+
+                    if (handler != null)
+                    {
+                        if (_options.WatchSlowMessage)
+                        {
+                            var start = DateTime.UtcNow;
+
+                            await handler.HandleAsync(message);
+
+                            var elapsed = DateTime.UtcNow - start;
+                            if (elapsed > _options.SlowMsgThreshold)
+                            {
+                                _logger.LogTrace("SLOW BUS MSG [{0}]: {1} - {2}ms. Handler: {3}.", Name, messageName, elapsed.TotalMilliseconds, (string)handler.GetType().Name);
+                            }
+                        }
+                        else
+                        {
+                            await handler.HandleAsync(message);
+                        }
+
+                        _logger.LogInformation($"MESSAGE BUS[{Name}]: the message[{messageName}] had been hanlded[{(string)handler.GetType().Name}].");
+                    }
+                    else
+                    {
+                        _logger.LogWarning($"the message[{messageName}] has not the handler.");
                     }
                 }
             }
