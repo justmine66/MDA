d5be9eee4fcd3224601a7a42efd603e3210527b2
justmine <justmine@DESKTOP-UILU6SC>
2018-08-24 07:29:11 -0700

    添加命令执行超时异常

---
+++ MDA/Commanding/Exceptions/CommandExecutedTimeoutException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MDA.Commanding.Exceptions
+{
+    public class CommandExecutedTimeoutException : Exception
+    {
+        public CommandExecutedTimeoutException() : base() { }
+        public CommandExecutedTimeoutException(string message) : 
+            base(message) { }
+        public CommandExecutedTimeoutException(string message, Exception innerException) : 
+            base(message, innerException) { }
+    }
+}
