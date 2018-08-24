494dc816857ddef5c74ac54fba053f332c3bae92
justmine <justmine@DESKTOP-UILU6SC>
2018-08-24 07:30:03 -0700

    添加命令服务接口

---
+++ MDA/Commanding/ICommandService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace MDA.Commanding
+{
+    /// <summary>
+    /// 命令服务。
+    /// </summary>
+    public interface ICommandService : ICommandBus
+    {
+        Task<CommandExcutedResult> ExcuteAsync<TCommand>(TCommand command)
+            where TCommand : ICommand;
+    }
+}
