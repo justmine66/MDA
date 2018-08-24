2c4f82200765d3dc6ab3a08b0e9a7146803db7d2
justmine <justmine@DESKTOP-UILU6SC>
2018-08-24 07:29:53 -0700

    添加命令总线接口

---
+++ MDA/Commanding/ICommandBus.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MDA.Commanding
+{
+    /// <summary>
+    /// 命令总线
+    /// </summary>
+    public interface ICommandBus
+    {
+        Task SendAsync<TCommand>(TCommand command)
+            where TCommand : ICommand;
+
+        Task SendAllAsync<TCommand>(IEnumerable<TCommand> command)
+            where TCommand : ICommand;
+    }
+}
