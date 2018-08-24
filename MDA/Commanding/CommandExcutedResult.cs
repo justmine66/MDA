b5cd96ff9bbe1753655a38494fe04c95426e3fed
justmine <justmine@DESKTOP-UILU6SC>
2018-08-24 07:29:25 -0700

    添加命令执行结果

---
+++ MDA/Commanding/CommandExcutedResult.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MDA.Commanding
+{
+    /// <summary>
+    /// 命令执行结果
+    /// </summary>
+    public class CommandExcutedResult
+    {
+        public CommandExcutedStatus ExcutedStatus { get; private set; }
+        public string CommandId { get; private set; }
+        public string Result { get; private set; }
+        public string ResultType { get; private set; }
+
+        public override string ToString()
+        {
+            return string.Format("CommandExcutedResult [CommandId={0},Status={1},Result={2},ResultType={3}]",
+                CommandId,
+                Enum.GetName(typeof(CommandExcutedStatus), ExcutedStatus),
+                Result,
+                ResultType);
+        }
+    }
+    /// <summary>
+    /// 命令执行状态
+    /// </summary>
+    public enum CommandExcutedStatus
+    {
+        None = 0,
+        Success = 1,
+        NothingChanged = 2,
+        Failed = 3
+    }
+}
