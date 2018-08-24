3aee5160e160817c471f2d4f228d8d9bc0c83dc0
李二毛 <lijun@geekbuy.com.cn>
2018-08-24 10:51:53 +0800

    添加MessageSubscriberCollection

---
+++ MDA/Messaging/IMessageSubscriberCollection.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace MDA.Messaging
+{
+    /// <summary>
+    /// A collection of message subsriber descriptor.
+    /// </summary>
+    public interface IMessageSubscriberCollection : IList<MessageSubscriberDescriptor>, ICollection<MessageSubscriberDescriptor>, IEnumerable<MessageSubscriberDescriptor>, IEnumerable
+    {
+
+    }
+}
---
+++ MDA/Messaging/Impl/DefaultMessageSubscriberCollection.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace MDA.Messaging.Impl
+{
+    public class DefaultMessageSubscriberCollection : IMessageSubscriberCollection
+    {
+        public IEnumerator<MessageSubscriberDescriptor> GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(MessageSubscriberDescriptor item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Contains(MessageSubscriberDescriptor item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void CopyTo(MessageSubscriberDescriptor[] array, int arrayIndex)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Remove(MessageSubscriberDescriptor item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int Count { get; }
+        public bool IsReadOnly { get; }
+        public int IndexOf(MessageSubscriberDescriptor item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Insert(int index, MessageSubscriberDescriptor item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public MessageSubscriberDescriptor this[int index]
+        {
+            get => throw new System.NotImplementedException();
+            set => throw new System.NotImplementedException();
+        }
+    }
+}
