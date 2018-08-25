4efacabe530aef0061e954708cc7bb02ac31be8b
justmine <justmine@DESKTOP-UILU6SC>
2018-08-25 01:53:33 -0700

    添加新的测试类

---
+++ MDA.Tests/Messaging/TestDynamicMessageHandler1.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using MDA.Messaging;
+
+namespace MDA.Tests.Messaging
+{
+    public class TestDynamicMessageHandler1 : IDynamicMessageHandler
+    {
+        public Task HandleAsync(dynamic message)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ MDA.Tests/Messaging/TestMessageHandler1.cs
@@ -0,0 +1,16 @@
+﻿using MDA.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDA.Tests.Messaging
+{
+    public class TestMessageHandler1 : IMessageHandler<TestMessage>
+    {
+        public Task HandleAsync(TestMessage message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
