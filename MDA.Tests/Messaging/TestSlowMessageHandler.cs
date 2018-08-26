a2d6b4412b85db1c146531aae1e1337fd4badfb5
justmine <justmine@DESKTOP-UILU6SC>
2018-08-25 23:45:51 -0700

    添加TestSlowMessageHandler

---
+++ MDA.Tests/Messaging/TestSlowMessageHandler.cs
@@ -0,0 +1,22 @@
+﻿using MDA.Messaging;
+using System;
+using System.Threading.Tasks;
+
+namespace MDA.Tests.Messaging
+{
+    public class TestSlowMessageHandler : IMessageHandler<TestMessage>
+    {
+        private readonly MessagingOptions _options;
+
+        public TestSlowMessageHandler(MessagingOptions options)
+        {
+            _options = options;
+        }
+
+        public async Task HandleAsync(TestMessage message)
+        {
+            var handleTime = _options.SlowMessageHandlerThreshold.Add(TimeSpan.FromSeconds(1));
+            await Task.Delay(handleTime);
+        }
+    }
+}
