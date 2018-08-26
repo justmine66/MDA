b88610f9c30c2c8d7920679d2244b77d2d4a2dfc
justmine <justmine@DESKTOP-UILU6SC>
2018-08-25 22:54:08 -0700

    重构InMemory_Message_Bus_Test

--- MDA.Tests/Messaging/InMemory_Message_Bus_Test.cs
+++ MDA.Tests/Messaging/InMemory_Message_Bus_Test.cs
@@ -1,10 +1,45 @@
-﻿namespace MDA.Tests.Messaging
+﻿using MDA.Messaging;
+using MDA.Messaging.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace MDA.Tests.Messaging
 {
     /// <summary>
     /// 内存消息总线测试类
     /// </summary>
     public class InMemory_Message_Bus_Test
     {
+        private readonly IMessageBus _messageBus;
 
+        public InMemory_Message_Bus_Test()
+        {
+            var provider = new ServiceCollection()
+                .AddLogging()
+                .AddMessaging(IsReigisterInMemoryBus: true)
+                .AddScoped<TestMessageHandler>()
+                .AddScoped<TestDynamicMessageHandler>()
+                .BuildServiceProvider();
+
+            _messageBus = provider.GetService<IMessageBus>();
+        }
+
+        [Fact(DisplayName = "发布一条类型化消息，消息处理者应该接收到。")]
+        public void After_Push_One_Typed_Message_Handler_Should_Recevied()
+        {
+            _messageBus.Subscribe<TestMessage, TestMessageHandler>();
+            _messageBus.PublishAsync(new TestMessage());
+
+            Assert.True(TestMessageHandler.ReceivedMessage);
+        }
+
+        [Fact(DisplayName = "发布一条动态消息，消息处理者应该接收到。")]
+        public void After_Push_One_Dynamic_Message_Handler_Should_Recevied()
+        {
+            _messageBus.SubscribeDynamic<TestDynamicMessageHandler>("TestMessage");
+            _messageBus.PublishDynamicAsync("TestMessage", new TestMessage());
+
+            Assert.True(TestDynamicMessageHandler.ReceivedMessage);
+        }
     }
 }
