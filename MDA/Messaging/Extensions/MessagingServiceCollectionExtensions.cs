bfdbb5aa9d7e6b64fd4342da4c5a5b45dc9d0ac7
justmine <justmine@DESKTOP-UILU6SC>
2018-08-25 22:48:49 -0700

    添加MessagingServiceCollectionExtensions，注入消息模块所需必要组件

---
+++ MDA/Messaging/Extensions/MessagingServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using MDA.Messaging.Impl;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MDA.Messaging.Extensions
+{
+    public static class MessagingServiceCollectionExtensions
+    {
+        public static IServiceCollection AddMessaging(this IServiceCollection services, bool IsReigisterInMemoryBus = false)
+        {
+            services.AddSingleton<MessagingOptions>();
+            services.AddSingleton<IMessageSubscriberCollection, DefaultMessageSubscriberCollection>();
+            services.AddSingleton<IMessageSubscriberManager, InMemoryMessageSubscriberManager>();
+
+            if (IsReigisterInMemoryBus)
+            {
+                services.AddSingleton<IMessageBus, InMemoryMessageBus>();
+            }
+
+            return services;
+        }
+    }
+}
