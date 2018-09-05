67e616de4a9b3ac97cb150cf7947de0b3ae05d3b
justmine <justmine@DESKTOP-UILU6SC>
2018-09-05 07:34:13 -0700

    添加StoredDomainEvent

---
+++ MDA.Event.Abstractions/IStoredDomainEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MDA.Event.Abstractions
+{
+    public class StoredDomainEvent
+    {
+        public string EventId { get; set; }
+        public int EventVersion { get; set; }
+        public string CommandId { get; set; }
+        public string AggregateRootId { get; set; }
+        public string AggregateRootTypeName { get; set; }
+        public DateTime? OccurredOn { get; set; }
+    }
+}
