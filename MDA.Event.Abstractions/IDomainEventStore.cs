a832634d5581a8b8c849e22fd1f5c1299269ad71
justmine <justmine@DESKTOP-UILU6SC>
2018-09-05 07:33:25 -0700

    添加IDomainEventStore

---
+++ MDA.Event.Abstractions/IDomainEventStore.cs
@@ -0,0 +1,37 @@
+﻿using MDA.Common;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MDA.Event.Abstractions
+{
+    public interface IDomainEventStore
+    {
+        Task<long> CountStoredEventsAsync();
+
+        Task<IDomainEvent[]> GetAllAggregateStoredEventsSinceAsync(
+            string aggregateRootId,
+            string aggregateRootTypeName,
+            int sequence);
+
+        Task<IDomainEvent[]> GetAllAggregateStoredEventsSinceAsync<TType>(
+            TType aggregateRootId, 
+            string aggregateRootTypeName, 
+            int sequence);
+
+        Task<IDomainEvent[]> GetAllAggregateStoredEventsBetweenAsync(
+            string aggregateRootId,
+            string aggregateRootTypeName,
+            long lowSequence,
+            long highSequence);
+
+        Task<IDomainEvent[]> GetAllAggregateStoredEventsBetweenAsync<TType>(
+            TType aggregateRootId,
+            string aggregateRootTypeName, 
+            long lowSequence, 
+            long highSequence);
+
+        Task<AsyncResult<DomainEventAppendResult>> AppendAsync(IDomainEvent domainEvent);
+
+        Task<AsyncResult<DomainEventAppendResult>> AppendAllAsync(IEnumerable<IDomainEvent> eventStream);
+    }
+}
