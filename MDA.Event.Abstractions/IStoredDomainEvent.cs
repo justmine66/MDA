f696abc7752bf3aa8b5d00fb0ce89dced681e9fe
justmine <justmine@DESKTOP-UILU6SC>
2018-09-06 08:11:49 -0700

    添加属性和纠正命名

--- MDA.Event.Abstractions/IStoredDomainEvent.cs
+++ MDA.Event.Abstractions/IStoredDomainEvent.cs
@@ -2,10 +2,17 @@
 
 namespace MDA.Event.Abstractions
 {
+    /// <summary>
+    /// 领域事件持久化对象
+    /// </summary>
     public class StoredDomainEvent
     {
         public string EventId { get; set; }
-        public int EventVersion { get; set; }
+        public string EventBody { get; set; }
+        /// <summary>
+        /// 事件序列号
+        /// </summary>
+        public int EventSequence { get; set; }
         public string CommandId { get; set; }
         public string AggregateRootId { get; set; }
         public string AggregateRootTypeName { get; set; }
