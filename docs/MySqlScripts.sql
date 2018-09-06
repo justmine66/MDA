3fe4bb14503fdc510264a06e9e50b0931fc2cf32
justmine <justmine@DESKTOP-UILU6SC>
2018-09-06 07:54:07 -0700

    纠正脚本

--- docs/MySqlScripts.sql
+++ docs/MySqlScripts.sql
@@ -4,11 +4,12 @@ USE `MDA`;
 
 CREATE TABLE `DomainEventStream` (
   `EventId` varchar(36) NOT NULL,
-  `EventVersion` int(11) NOT NULL,
+  `EventBody` varchar(4000) NOT NULL,
+  `EventSequence` int(11) NOT NULL,
   `CommandId` varchar(36) NOT NULL,
   `AggregateRootId` varchar(36) NOT NULL,
   `AggregateRootTypeName` varchar(256) NOT NULL,
   `OccurredOn` datetime NOT NULL,
-  UNIQUE KEY `IX_EventStream_AggId_Version` (`AggregateRootId`,`EventVersion`),
+  UNIQUE KEY `IX_EventStream_AggId_Version` (`AggregateRootId`,`EventId`,`EventSequence`),
   UNIQUE KEY `IX_EventStream_AggId_CommandId` (`AggregateRootId`,`CommandId`)
 )ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
