5b8f04213ff61407e82bcd4105617f7cbd2c4023
justmine <justmine@DESKTOP-UILU6SC>
2018-09-06 08:10:44 -0700

    添加IEventSerializer

---
+++ MDA.Event.Abstractions/IEventSerializer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MDA.Event.Abstractions
+{
+    public interface IEventSerializer
+    {
+        T Deserialize<T>(string serialization);
+        object Deserialize(string serialization, Type type);
+        string Serialize(IDomainEvent domainEvent);
+    }
+}
