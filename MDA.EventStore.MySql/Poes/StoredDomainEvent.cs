53b58ab46985e174cefb5b024f9d56b7717c80b1
justmine <justmine@DESKTOP-UILU6SC>
2018-09-07 23:40:58 -0700

    添加ToString()

--- MDA.EventStore.MySql/Poes/StoredDomainEvent.cs
+++ MDA.EventStore.MySql/Poes/StoredDomainEvent.cs
@@ -14,5 +14,10 @@ namespace MDA.EventStore.MySql.Poes
         public string AggregateRootId { get; set; }
         public string AggregateRootTypeName { get; set; }
         public DateTime? OccurredOn { get; set; }
+
+        public override string ToString()
+        {
+            return $"StoredDomainEvent [EventId: {EventId},EventSequence: {EventSequence},CommandId: {CommandId},AggregateRootId: {AggregateRootId},AggregateRootTypeName: {AggregateRootTypeName},OccurredOn: {OccurredOn},EventBody: {EventBody}]";
+        }
     }
 }
