d6e0756cf91f1e8228e46e501e14f06d181f0f5a
justmine <justmine@DESKTOP-UILU6SC>
2018-09-08 08:00:50 -0700

    添加扩展DbTransactionExtension

---
+++ MDA.Common/Extensions/DbTransactionExtension.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Common;
+using System.Threading.Tasks;
+
+namespace MDA.Common.Extensions
+{
+    public static class DbTransactionExtension
+    {
+        public static Task CommitAsync(this DbTransaction transaction)
+        {
+            return Task.Run(() => { transaction.Commit(); });
+        }
+
+        public static Task RollbackAsync(this DbTransaction transaction)
+        {
+            return Task.Run(() => { transaction.Rollback(); });
+        }
+    }
+}
