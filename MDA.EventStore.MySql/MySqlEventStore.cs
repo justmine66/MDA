b88bb559f6026cae2efef679cb6f2b917b2f0676
justmine <justmine@DESKTOP-UILU6SC>
2018-09-08 02:38:32 -0700

    删除不必要的命名空间

--- MDA.EventStore.MySql/MySqlEventStore.cs
+++ MDA.EventStore.MySql/MySqlEventStore.cs
@@ -6,7 +6,6 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using MySql.Data.MySqlClient;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
