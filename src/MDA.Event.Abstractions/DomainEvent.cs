631043f7ce0b53225a2fa7418dee2efc88362d48
justmine <justmine@DESKTOP-UILU6SC>
2018-09-09 06:35:18 -0700

    添加DomainEvent

---
+++ src/MDA.Event.Abstractions/DomainEvent.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace MDA.Event.Abstractions
+{
+    public abstract class DomainEvent : IDomainEvent
+    {
+        public DomainEvent() { }
+
+        public DomainEvent(
+            string commandId, 
+            string aggregateRootId, 
+            string aggregateRootTypeName,
+            int eventSequence)
+        {
+            CommandId = commandId;
+            AggregateRootId = aggregateRootId;
+            AggregateRootTypeName = aggregateRootTypeName;
+            Sequence = eventSequence;
+            Id = Guid.NewGuid().ToString("N");
+            Timestamp = DateTime.Now;
+        }
+
+        public string CommandId { get; set; }
+        public string AggregateRootId { get; set; }
+        public string AggregateRootTypeName { get; set; }
+        public int Sequence { get; set; }
+        public string Id { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+
+    public abstract class DomainEvent<TAggregateRootIdType> : IDomainEvent<TAggregateRootIdType>
+    {
+        public DomainEvent(
+            string commandId,
+            TAggregateRootIdType aggregateRootId,
+            string aggregateRootTypeName,
+            int eventSequence)
+        {
+            CommandId = commandId;
+            AggregateRootId = aggregateRootId;
+            AggregateRootTypeName = aggregateRootTypeName;
+            Sequence = eventSequence;
+            Id = Guid.NewGuid().ToString("N");
+            Timestamp = DateTime.Now;
+        }
+
+        public string CommandId { get; set; }
+        public TAggregateRootIdType AggregateRootId { get; set; }
+        public string AggregateRootTypeName { get; set; }
+        public int Sequence { get; set; }
+        public string Id { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
