09c6e8b5503077414b4f384fba841ff92ccd7414
justmine <justmine@DESKTOP-UILU6SC>
2018-09-08 21:05:13 -0700

    添加Runner

---
+++ src/MDA.Common/Scheduling/IRunner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MDA.Common.Scheduling
+{
+    /// <summary>
+    /// 按固定的间隔不断地运行任务的运行器。
+    /// </summary>
+    public interface IRunner
+    {
+        void Run(TimeSpan interval, Action callback);
+        void Run<TState>(TimeSpan interval, Action<TState> callback, TState state);
+    }
+}
---
+++ src/MDA.Common/Scheduling/Runner.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Primitives;
+using System;
+using System.Threading;
+
+namespace MDA.Common.Scheduling
+{
+    public class Runner : IRunner
+    {
+        public void Run(TimeSpan interval, Action callback)
+        {
+            ChangeToken.OnChange(() => new CancellationChangeToken(new CancellationTokenSource(interval).Token), callback);
+        }
+        public void Run<TState>(TimeSpan interval, Action<TState> callback, TState state)
+        {
+            ChangeToken.OnChange(() => new CancellationChangeToken(new CancellationTokenSource(interval).Token), callback, state);
+        }
+    }
+}
