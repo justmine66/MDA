c417c4203971b257feda4ead66fb1586533cb465
justmine <justmine@DESKTOP-UILU6SC>
2018-09-08 23:58:53 -0700

    添加EventServiceCollectionExtension

---
+++ src/MDA.Event.Abstractions/Extensions/EventServiceCollectionExtension.cs
@@ -0,0 +1,15 @@
+﻿using MDA.Event.Abstractions.Impl;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MDA.Event.Abstractions.Extensions
+{
+    public static class EventServiceCollectionExtension
+    {
+        public static IServiceCollection AddMySqlDomainEventStorage(this IServiceCollection services)
+        {
+            services.AddSingleton<IEventSerializer, EventSerializer>();
+
+            return services;
+        }
+    }
+}
