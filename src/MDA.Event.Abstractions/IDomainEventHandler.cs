1470e18bb1045294071e230a2cffdd19f177b7ad
justmine <justmine@DESKTOP-UILU6SC>
2018-09-08 20:42:29 -0700

    重命名和添加方法

--- src/MDA.Event.Abstractions/IDomainEventHandler.cs
+++ src/MDA.Event.Abstractions/IDomainEventHandler.cs
@@ -4,12 +4,13 @@ namespace MDA.Event.Abstractions
 {
     public interface IDomainEventHandler
     {
+        Task HandleAsync(IDomainEvent @event);
     }
 
     public interface IDomainEventHandler<in TIDomainEvent>
         : IDomainEventHandler
         where TIDomainEvent : IDomainEvent
     {
-        Task HandleAsync(IDomainEvent @event);
+        Task HandleAsync(TIDomainEvent @event);
     }
 }
