ecd32645fec42522d318cb288e01f5cabaa91639
justmine <justmine@DESKTOP-UILU6SC>
2018-09-08 23:33:24 -0700

    添加事件储存数据库脚本

---
+++ docs/DomainEventStore/SqlServerDbScripts.sql
@@ -0,0 +1,18 @@
+﻿CREATE DATABASE [MDA]
+GO
+USE [MDA]
+GO
+
+CREATE TABLE [dbo].[DomainEventStream] (
+  [EventId] NVARCHAR(36) NOT NULL,
+  [EventBody] NVARCHAR(4000) NOT NULL,
+  [EventSequence] INT NOT NULL,
+  [CommandId] NVARCHAR(36) NOT NULL,
+  [AggregateRootId] NVARCHAR(36) NOT NULL,
+  [AggregateRootTypeName] NVARCHAR(256) NOT NULL,
+  [OccurredOn] DATETIME NOT NULL
+)
+GO
+CREATE UNIQUE INDEX [IX_DomainEventStream_AggId_Version]   ON [dbo].[DomainEventStream] ([AggregateRootId] ASC, [EventId] ASC, [EventSequence] ASC)
+GO
+CREATE UNIQUE INDEX [IX_DomainEventStream_AggId_CommandId] ON [dbo].[DomainEventStream] ([AggregateRootId] ASC, [CommandId] ASC)
