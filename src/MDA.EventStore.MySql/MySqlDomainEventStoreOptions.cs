15ab13c88220feda372a0f1838f478046382ec68
justmine <justmine@DESKTOP-UILU6SC>
2018-09-08 23:34:26 -0700

    添加AppendEventStreamTimeoutInSeconds

--- src/MDA.EventStore.MySql/MySqlDomainEventStoreOptions.cs
+++ src/MDA.EventStore.MySql/MySqlDomainEventStoreOptions.cs
@@ -11,5 +11,10 @@
         /// 根据算法 hash(AggregateRootShardTableCount)，聚合根产生的领域事件数据将均匀地储存到分片表中。
         /// </remarks>
         public int AggregateRootShardTableCount { get; set; }
+
+        /// <summary>
+        /// 批量附加一个聚合根的所有事件(事件流)超时时间。单位秒。默认30秒。
+        /// </summary>
+        public int AppendEventStreamTimeoutInSeconds { get; set; }
     }
 }
