d508d96e06e99a9ec13bedbf9bf48e215ac7b642
李二毛 <lijun@geekbuy.com.cn>
2018-09-10 09:15:44 +0800

    修改注释说明

--- src/MDA.EventStore.SqlServer/SqlServerDomainEventStoreOptions.cs
+++ src/MDA.EventStore.SqlServer/SqlServerDomainEventStoreOptions.cs
@@ -8,7 +8,7 @@
         /// 聚合根分片表数量。默认值：5。
         /// </summary>
         /// <remarks>
-        /// 根据算法 hash(AggregateRootShardTableCount)，聚合根产生的领域事件数据将均匀地储存到分片表中。
+        /// 根据算法 hash(AggregateRootId)%AggregateRootShardTableCount，聚合根产生的领域事件数据将均匀地储存到分片表中。
         /// </remarks>
         public int AggregateRootShardTableCount { get; set; }
 
