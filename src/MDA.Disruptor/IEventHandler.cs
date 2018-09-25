564b0177352c74e2e15fc3347e676ac593eb205f
李二毛 <lijun@geekbuy.com.cn>
2018-09-25 15:15:11 +0800

    重命名

--- src/MDA.Disruptor/IEventHandler.cs
+++ src/MDA.Disruptor/IEventHandler.cs
@@ -3,9 +3,9 @@
     /// <summary>
     /// Callback interface to be implemented for processing events as they become available in the <see cref="RingBuffer{T}"/>
     /// </summary>
-    /// <typeparam name="T">Type of events for sharing during exchange or parallel coordination of an event.</typeparam>
+    /// <typeparam name="TData">Type of events for sharing during exchange or parallel coordination of an event.</typeparam>
     /// <remarks>See <see cref="IBatchEventProcessor{T}.SetExceptionHandler"/> if you want to handle exceptions propagated out of the handler.</remarks>
-    public interface IEventHandler<in T>
+    public interface IEventHandler<in TData>
     {
         /// <summary>
         /// Called when a publisher has committed an event to the <see cref="RingBuffer{T}"/>. The <see cref="IBatchEventProcessor{T}"/> will
@@ -18,6 +18,6 @@
         /// <param name="data">Data committed to the <see cref="RingBuffer{T}"/></param>
         /// <param name="sequence">Sequence number committed to the <see cref="RingBuffer{T}"/></param>
         /// <param name="endOfBatch">flag to indicate if this is the last event in a batch from the <see cref="RingBuffer{T}"/></param>
-        void OnEvent(T data, long sequence, bool endOfBatch);
+        void OnEvent(TData data, long sequence, bool endOfBatch);
     }
 }
