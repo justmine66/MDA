972048b41c497976e10e64e79154042ba3e47c27
李二毛 <lijun@geekbuy.com.cn>
2018-09-26 20:09:23 +0800

    重构RingBuffer

--- src/MDA.Disruptor/RingBuffer.cs
+++ src/MDA.Disruptor/RingBuffer.cs
@@ -1,46 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace MDA.Disruptor
 {
-    public abstract class RingBufferPad
+    public class RingBuffer<TEvent>
     {
-        protected long p1, p2, p3, p4, p5, p6, p7;
-    }
-
-    public class RingBufferFields<E> : RingBufferPad
-    {
-        private static readonly int BUFFER_PAD;
-        private static readonly long REF_ARRAY_BASE;
-        private static readonly int REF_ELEMENT_SHIFT;
-
-        static RingBufferFields()
-        {
-            var scale = IntPtr.Size;
-
-            if (4 == scale)
-            {
-                REF_ELEMENT_SHIFT = 2;
-            }
-            else if (8 == scale)
-            {
-                REF_ELEMENT_SHIFT = 3;
-            }
-            else
-            {
-                throw new PlatformNotSupportedException("Unknown pointer size");
-            }
-
-            BUFFER_PAD = 128 / scale;
-
-        }
-
-        private readonly long indexMask;
-        private readonly Object[] entries;
-        protected readonly int bufferSize;
-        protected readonly ISequencer sequencer;
-
 
     }
-
-    public sealed class RingBuffer<T> { }
 }
