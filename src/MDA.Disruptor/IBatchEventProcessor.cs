b9a32b9628c590a98d33aff273d83c5e3dcd7cea
李二毛 <lijun@geekbuy.com.cn>
2018-09-29 18:18:26 +0800

    添加IBatchEventProcessor

--- src/MDA.Disruptor/IBatchEventProcessor.cs
+++ src/MDA.Disruptor/IBatchEventProcessor.cs
@@ -1,7 +1,23 @@
-﻿namespace MDA.Disruptor
-{
-    public interface IBatchEventProcessor<T>
-    {
+﻿using System;
 
+namespace MDA.Disruptor
+{
+    /// <summary>
+    /// Interface for <see cref="BatchEventProcessor{T,TDataProvider,TSequenceBarrier,TEventHandler,TBatchStartAware}"/>.
+    /// </summary>
+    /// <typeparam name="T">the type of event used.</typeparam>
+    public interface IBatchEventProcessor<T> : IEventProcessor
+    {
+        /// <summary>
+        /// Waits before the event processor enters the <see cref="IEventProcessor.IsRunning"/> state.
+        /// </summary>
+        /// <param name="timeout">maximum wait duration</param>
+        void WaitUntilStarted(TimeSpan timeout);
+
+        /// <summary>
+        /// Set a new <see cref="IExceptionHandler{T}"/> for handling exceptions propagated out of the <see cref="IEventHandler{T}"/>
+        /// </summary>
+        /// <param name="exceptionHandler">exceptionHandler to replace the existing exceptionHandler.</param>
+        void SetExceptionHandler(IExceptionHandler<T> exceptionHandler);
     }
 }
