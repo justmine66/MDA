cea9068c488fbd51e0f0ada0d7ce67fca8262969
justmine <justmine@DESKTOP-UILU6SC>
2018-10-10 07:55:17 -0700

    添加注释

--- src/MDA.Disruptor/IWaitStrategy.cs
+++ src/MDA.Disruptor/IWaitStrategy.cs
@@ -6,12 +6,12 @@
     public interface IWaitStrategy
     {
         /// <summary>
-        ///  Wait for the given sequence to be available. It is possible for this method to return a value less than the sequence number supplied depending on the implementation of the WaitStrategy. A common use for this is to signal a timeout.Any EventProcessor that is using a WaitStrategy to get notifications about message becoming available should remember to handle this case.  The <see cref="IBatchEventProcessor{T}"/> explicitly handles this case and will signal a timeout if required.
+        /// Wait for the given sequence to be available. It is possible for this method to return a value less than the sequence number supplied depending on the implementation of the WaitStrategy. A common use for this is to signal a timeout.Any EventProcessor that is using a WaitStrategy to get notifications about message becoming available should remember to handle this case. The <see cref="IBatchEventProcessor{T}"/> explicitly handles this case and will signal a timeout if required.
         /// </summary>
-        /// <param name="sequence"></param>
-        /// <param name="cursor"></param>
-        /// <param name="dependentSequence"></param>
-        /// <param name="barrier"></param>
+        /// <param name="sequence">to be waited on.</param>
+        /// <param name="cursor">the main sequence from ringbuffer. Wait/notify strategies will need this as it's the only sequence that is also notified upon update.</param>
+        /// <param name="dependentSequence">on which to wait.</param>
+        /// <param name="barrier">the processor is waiting on.</param>
         /// <returns></returns>
         long WaitFor(long sequence, ISequence cursor, ISequence dependentSequence, ISequenceBarrier barrier);
 
