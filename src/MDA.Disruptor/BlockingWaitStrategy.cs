1b972c3c4c43b40e73b1dc34b134826f9eb64a0a
justmine <justmine@DESKTOP-UILU6SC>
2018-10-10 07:54:15 -0700

    添加BlockingWaitStrategy

---
+++ src/MDA.Disruptor/BlockingWaitStrategy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MDA.Disruptor
+{
+    public class BlockingWaitStrategy : IWaitStrategy
+    {
+        public void SignalAllWhenBlocking()
+        {
+            throw new NotImplementedException();
+        }
+
+        public long WaitFor(long sequence, ISequence cursor, ISequence dependentSequence, ISequenceBarrier barrier)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
