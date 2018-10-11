8fa2b5d47882d8a3430547afafb66e9a71e6adfb
justmine <justmine@DESKTOP-UILU6SC>
2018-10-11 07:52:55 -0700

    实现BlockingWaitStrategy

--- src/MDA.Disruptor/BlockingWaitStrategy.cs
+++ src/MDA.Disruptor/BlockingWaitStrategy.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.Threading;
 
 namespace MDA.Disruptor
 {
+    /// <summary>
+    /// Blocking strategy that uses a lock and condition variable for <see cref="IEventProcessor"/>s waiting on a barrier.
+    /// </summary>
+    /// <remarks>This strategy can be used when throughput and low-latency are not as important as CPU resource.</remarks>
     public class BlockingWaitStrategy : IWaitStrategy
     {
+        private readonly Object mutex = new Object();
+
         public void SignalAllWhenBlocking()
         {
             throw new NotImplementedException();
@@ -11,7 +18,27 @@ namespace MDA.Disruptor
 
         public long WaitFor(long sequence, ISequence cursor, ISequence dependentSequence, ISequenceBarrier barrier)
         {
-            throw new NotImplementedException();
+            if (cursor.GetValue() < sequence)
+            {
+                lock(mutex)
+                {
+                    while (cursor.GetValue() < sequence)
+                    {
+                        barrier.CheckAlert();
+                        Monitor.Wait(mutex);
+                    }
+                }
+            }
+
+            long availableSequence;
+            var aggressiveSpinWait = new AggressiveSpinWait();
+            while ((availableSequence = dependentSequence.GetValue()) < sequence)
+            {
+                barrier.CheckAlert();
+                aggressiveSpinWait.SpinOnce();
+            }
+
+            return availableSequence;
         }
     }
 }
