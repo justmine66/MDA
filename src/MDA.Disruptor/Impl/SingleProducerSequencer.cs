425adf792a992f65c880313a3418c9c73b860228
justmine <justmine@DESKTOP-UILU6SC>
2018-10-12 07:27:22 -0700

    添加方法和修改访问修饰符

--- src/MDA.Disruptor/Impl/SingleProducerSequencer.cs
+++ src/MDA.Disruptor/Impl/SingleProducerSequencer.cs
@@ -19,14 +19,69 @@
         {
         }
 
-        long nextValue = Sequence.InitialValue;
-        long cachedValue = Sequence.InitialValue;
+        protected long nextValue = Sequence.InitialValue;
+        protected long cachedValue = Sequence.InitialValue;
     }
 
     public class SingleProducerSequencer : SingleProducerSequencerFields
     {
         public SingleProducerSequencer(int bufferSize, IWaitStrategy waitStrategy) : base(bufferSize, waitStrategy)
         {
+        }
+
+        public override void Claim(long sequence)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override long GetHighestPublishedSequence(long nextSequence, long availableSequence)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override long GetRemainingCapacity()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool HasAvailableCapacity(int requiredCapacity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool IsAvailable(long sequence)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override long Next()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override long Next(int n)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Publish(long sequence)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Publish(long lo, long hi)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool TryNext(out long sequence)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool TryNext(int n, out long sequence)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
