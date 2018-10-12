e9598dba7ecd88a76855a5e366ec9cd4e66fdffd
justmine <justmine@DESKTOP-UILU6SC>
2018-10-12 07:38:49 -0700

    添加YieldingWaitStrategy

---
+++ src/MDA.Disruptor/YieldingWaitStrategy.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+
+namespace MDA.Disruptor
+{
+    /// <summary>
+    /// Yielding strategy that uses a Thread.yield() for <see cref="IEventProcessor"/>s waiting on a barrier after an initially spinning.
+    /// </summary>
+    /// <remarks>This strategy will use 100% CPU, but will more readily give up the CPU than a busy spin strategy if other threads require CPU resource.</remarks>
+    public class YieldingWaitStrategy : IWaitStrategy
+    {
+        private const int _spinTries = 100;
+
+        public void SignalAllWhenBlocking()
+        {
+            throw new NotImplementedException();
+        }
+
+        public long WaitFor(long sequence, ISequence cursor, ISequence dependentSequence, ISequenceBarrier barrier)
+        {
+            long availableSequence;
+            int counter = _spinTries;
+
+            while ((availableSequence = dependentSequence.GetValue()) < sequence)
+            {
+                counter = ApplyWaitMethod(barrier, counter);
+            }
+
+            return availableSequence;
+        }
+
+        private int ApplyWaitMethod(ISequenceBarrier barrier, int counter)
+        {
+            barrier.CheckAlert();
+
+            if (0 == counter)
+            {
+                Thread.Yield();
+            }
+            else
+            {
+                --counter;
+            }
+
+            return counter;
+        }
+    }
+}
