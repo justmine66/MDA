ab3fe95e595737d3bc64e21a98f61d6eeb3f13d8
justmine <justmine@DESKTOP-UILU6SC>
2018-10-12 07:21:18 -0700

    添加SingleProducerSequencer

---
+++ src/MDA.Disruptor/SingleProducerSequencer.cs
@@ -0,0 +1,31 @@
+﻿using MDA.Disruptor.Impl;
+
+namespace MDA.Disruptor
+{
+    public abstract class SingleProducerSequencerPad : AbstractSequencer
+    {
+        protected long p1, p2, p3, p4, p5, p6, p7;
+
+        public SingleProducerSequencerPad(int bufferSize, IWaitStrategy waitStrategy) : base(bufferSize, waitStrategy)
+        {
+
+        }
+    }
+
+    public abstract class SingleProducerSequencerFields : SingleProducerSequencerPad
+    {
+        public SingleProducerSequencerFields(int bufferSize, IWaitStrategy waitStrategy) : base(bufferSize, waitStrategy)
+        {
+        }
+
+        long nextValue = Sequence.InitialValue;
+        long cachedValue = Sequence.InitialValue;
+    }
+
+    public class SingleProducerSequencer : SingleProducerSequencerFields
+    {
+        public SingleProducerSequencer(int bufferSize, IWaitStrategy waitStrategy) : base(bufferSize, waitStrategy)
+        {
+        }
+    }
+}
