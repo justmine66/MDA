fbe8a252990f5b15169433d736e31bb32e82ce54
justmine <justmine@DESKTOP-UILU6SC>
2018-10-14 07:48:00 -0700

    SequenceUpdater

---
+++ tests/MDA.Tests/Disruptor/Support/SequenceUpdater.cs
@@ -0,0 +1,41 @@
+﻿using MDA.Disruptor;
+using MDA.Disruptor.Impl;
+using System;
+using System.Threading;
+
+namespace MDA.Tests.Disruptor.Support
+{
+    public class SequenceUpdater
+    {
+        public ISequence sequence = new Sequence();
+        private CountdownEvent _barrier = new CountdownEvent(2);
+        private long _sleepTime;
+        private IWaitStrategy _waitStrategy;
+
+        public SequenceUpdater(long sleepTime, IWaitStrategy waitStrategy)
+        {
+            _sleepTime = sleepTime;
+            _waitStrategy = waitStrategy;
+        }
+
+        public void Run()
+        {
+            try
+            {
+                _barrier.Signal();
+                _barrier.Wait();
+                if (0 != _sleepTime)
+                {
+                    Thread.Sleep((int)_sleepTime);
+                }
+
+                sequence.IncrementAndGet();
+                _waitStrategy.SignalAllWhenBlocking();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
