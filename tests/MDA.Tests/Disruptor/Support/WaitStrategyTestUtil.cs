49e1cfda809422c365e8053237b2df91fe83d0c7
justmine <justmine@DESKTOP-UILU6SC>
2018-10-14 07:48:10 -0700

    WaitStrategyTestUtil

---
+++ tests/MDA.Tests/Disruptor/Support/WaitStrategyTestUtil.cs
@@ -0,0 +1,11 @@
+﻿using MDA.Disruptor;
+
+namespace MDA.Tests.Disruptor.Support
+{
+    public class WaitStrategyTestUtil
+    {
+        public static void AssertWaitForWithDelayOf(long sleepTimeMillis, IWaitStrategy waitStrategy) {
+
+        }
+    }
+}
