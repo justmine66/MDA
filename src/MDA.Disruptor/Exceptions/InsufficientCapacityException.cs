1db0f22b2ac78d7f31958ae1cadfcdf50dddbe6b
justmine <justmine@DESKTOP-UILU6SC>
2018-10-15 05:54:55 -0700

    添加InsufficientCapacityException

---
+++ src/MDA.Disruptor/Exceptions/InsufficientCapacityException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MDA.Disruptor.Exceptions
+{
+    public class InsufficientCapacityException : Exception
+    {
+        public static readonly InsufficientCapacityException Instance = new InsufficientCapacityException();
+
+        private InsufficientCapacityException()
+        {
+            // Singleton
+        }
+
+        /// <summary>
+        /// throw an exception.
+        /// </summary>
+        public static void Throw()
+        {
+            throw Instance;
+        }
+    }
+}
