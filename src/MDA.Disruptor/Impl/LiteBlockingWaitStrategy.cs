1252687005378e6c52cf214c384954c863ee0588
justmine <justmine@DESKTOP-UILU6SC>
2018-10-16 07:29:32 -0700

    添加LiteBlockingWaitStrategy

---
+++ src/MDA.Disruptor/Impl/LiteBlockingWaitStrategy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MDA.Disruptor.Impl
+{
+    /// <summary>
+    /// Variation of the {@link TimeoutBlockingWaitStrategy} that attempts to elide conditional wake-ups when the lock is uncontended.
+    /// </summary>
+    public class LiteBlockingWaitStrategy : IWaitStrategy
+    {
+        public void SignalAllWhenBlocking()
+        {
+            throw new NotImplementedException();
+        }
+
+        public long WaitFor(long sequence, ISequence cursor, ISequence dependentSequence, ISequenceBarrier barrier)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
