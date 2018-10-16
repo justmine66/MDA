2dcace55ccd1faa7b0a80de9512a0ad26278e012
justmine <justmine@DESKTOP-UILU6SC>
2018-10-16 07:29:44 -0700

    添加LiteTimeoutBlockingWaitStrategy

---
+++ src/MDA.Disruptor/Impl/LiteTimeoutBlockingWaitStrategy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MDA.Disruptor.Impl
+{
+    public class LiteTimeoutBlockingWaitStrategy : IWaitStrategy
+    {
+        public void SignalAllWhenBlocking()
+        {
+            throw new NotImplementedException();
+        }
+
+        public long WaitFor(long sequence, ISequence cursor, ISequence dependentSequence, ISequenceBarrier barrier)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
