73e7e93f7c44f5f0e4f45429229b9efd00d62b38
justmine <justmine@DESKTOP-UILU6SC>
2018-10-16 07:29:20 -0700

    添加IgnoreExceptionHandler

---
+++ src/MDA.Disruptor/Impl/IgnoreExceptionHandler.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace MDA.Disruptor.Impl
+{
+    /// <summary>
+    /// Convenience implementation of an exception handler that using standard logging to log the exception as <see cref="LogLevel.Information"/>. 
+    /// </summary>
+    public class IgnoreExceptionHandler : IExceptionHandler<object>
+    {
+        private readonly ILogger _logger;
+        public IgnoreExceptionHandler(ILogger<IgnoreExceptionHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public void HandleEventException(Exception ex, long sequence, object @event)
+        {
+            _logger.LogInformation("Exception processing: " + sequence + " " + @event, ex);
+        }
+
+        public void HandleOnShutdownException(Exception ex)
+        {
+            _logger.LogInformation("Exception during onStart()", ex);
+        }
+
+        public void HandleOnStartException(Exception ex)
+        {
+            _logger.LogInformation("Exception during onShutdown()", ex);
+        }
+    }
+}
