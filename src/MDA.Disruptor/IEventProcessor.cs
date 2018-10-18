d14e58deca4dc74cc7cdc8561520d631f09ed56d
李二毛 <lijun@geekbuy.com.cn>
2018-10-18 17:43:27 +0800

    修改注释和移除方法

--- src/MDA.Disruptor/IEventProcessor.cs
+++ src/MDA.Disruptor/IEventProcessor.cs
@@ -1,9 +1,7 @@
 ﻿namespace MDA.Disruptor
 {
     /// <summary>
-    /// An IEventProcessor needs to poll for events from the <see cref="RingBuffer{T}"/>
-    /// using the appropriate wait strategy. It is unlikely that you will need to implement this interface yourself.
-    /// Look at using the <see cref="IEventHandler{T}"/> interface along with the pre-supplied BatchEventProcessor in the first
+    /// An EventProcessor needs to be an implementation of a runnable that will poll for events from the <see cref="RingBuffer{TEvent}"/> using the appropriate wait strategy. It is unlikely that you will need to implement this interface yourself. Look at using the <see cref="IEventHandler{TEvent}"/> interface along with the pre-supplied BatchEventProcessor in the first instance.
     /// </summary>
     public interface IEventProcessor
     {
@@ -17,11 +15,6 @@
         /// It will call <see cref="ISequenceBarrier.Alert"/> to notify the thread to check status.
         /// </summary>
         void Halt();
-
-        /// <summary>
-        /// Starts this instance.
-        /// </summary>
-        void Run();
 
         /// <summary>
         /// Gets if the processor is running.
