34a934fd43e2ef2f3642f633ddea15da12890623
justmine <justmine@DESKTOP-UILU6SC>
2018-10-18 05:31:32 -0700

    添加NoOpEventProcessor

---
+++ src/MDA.Disruptor/Impl/NoOpEventProcessor .cs
@@ -0,0 +1,61 @@
+﻿using MDA.Disruptor.Exceptions;
+using System.Threading;
+
+namespace MDA.Disruptor.Impl
+{
+    /// <summary p="This is useful in tests or for pre-filling a <see cref="RingBuffer"/> from a publisher.">
+    /// No operation version of a <see cref="IEventProcessor"/> that simply tracks a <see cref="ISequence"/>.
+    /// </summary>
+    public class NoOpEventProcessor<T> : IEventProcessor
+    {
+        private readonly SequencerFollowingSequence _sequence;
+
+        private volatile int _running = 0;
+
+        private NoOpEventProcessor(RingBuffer<T> sequencer)
+        {
+            _sequence = new SequencerFollowingSequence(sequencer);
+        }
+
+        public ISequence GetSequence()
+        {
+            return _sequence;
+        }
+
+        public void Halt()
+        {
+            Interlocked.Exchange(ref _running, 0);
+        }
+
+        public bool IsRunning()
+        {
+            return _running == 1;
+        }
+
+        public void Run()
+        {
+            if (Interlocked.CompareExchange(ref _running, 0, 1) == 1)
+            {
+                throw new IllegalStateException("Thread is already running.");
+            }
+        }
+
+        /// <summary>
+        /// Sequence that follows (by wrapping) another sequence.
+        /// </summary>
+        private sealed class SequencerFollowingSequence : Sequence
+        {
+            private readonly RingBuffer<T> _sequencer;
+
+            public SequencerFollowingSequence(RingBuffer<T> sequencer)
+            {
+                _sequencer = sequencer;
+            }
+
+            public override long GetValue()
+            {
+                return _sequencer.GetCursor();
+            }
+        }
+    }
+}
