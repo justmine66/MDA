00757a8d035cc67e87fc11c87f0d13ec9d321510
李二毛 <lijun@geekbuy.com.cn>
2018-10-18 10:50:06 +0800

    规范常量命名

--- src/MDA.Disruptor/AggressiveSpinWait.cs
+++ src/MDA.Disruptor/AggressiveSpinWait.cs
@@ -8,20 +8,20 @@ namespace MDA.Disruptor
     /// </summary>
     public struct AggressiveSpinWait
     {
-        private static readonly bool _isSingleProcessor = Environment.ProcessorCount == 1;
-        private const int _yieldThreshold = 10;
-        private const int _sleep0EveryHowManyTimes = 5;
+        private static readonly bool IsSingleProcessor = Environment.ProcessorCount == 1;
+        private const int YieldThreshold = 10;
+        private const int Sleep0EveryHowManyTimes = 5;
         private int _count;
 
-        private bool NextSpinWillYield => _count > _yieldThreshold || _isSingleProcessor;
+        private bool NextSpinWillYield => _count > YieldThreshold || IsSingleProcessor;
 
         public void SpinOnce()
         {
             if (NextSpinWillYield)
             {
-                int yieldsSoFar = (_count >= _yieldThreshold ? _count - _yieldThreshold : _count);
+                int yieldsSoFar = (_count >= YieldThreshold ? _count - YieldThreshold : _count);
 
-                if ((yieldsSoFar % _sleep0EveryHowManyTimes) == (_sleep0EveryHowManyTimes - 1))
+                if ((yieldsSoFar % Sleep0EveryHowManyTimes) == (Sleep0EveryHowManyTimes - 1))
                 {
                     Thread.Sleep(0);
                 }
@@ -35,7 +35,7 @@ namespace MDA.Disruptor
                 Thread.SpinWait(4 << _count);
             }
 
-            _count = (_count == int.MaxValue ? _yieldThreshold : _count + 1);
+            _count = (_count == int.MaxValue ? YieldThreshold : _count + 1);
         }
     }
 }
