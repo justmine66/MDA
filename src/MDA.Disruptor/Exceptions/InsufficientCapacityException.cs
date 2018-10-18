36e763d008583d21a5bcc8a4cb12db52a044ead0
李二毛 <lijun@geekbuy.com.cn>
2018-10-18 18:11:02 +0800

    添加注释

--- src/MDA.Disruptor/Exceptions/InsufficientCapacityException.cs
+++ src/MDA.Disruptor/Exceptions/InsufficientCapacityException.cs
@@ -2,6 +2,9 @@
 
 namespace MDA.Disruptor.Exceptions
 {
+    /// <summary>
+    /// Exception thrown when it is not possible to insert a value into the ring buffer without it wrapping the consuming sequences. Used specifically when claiming with the <see cref="RingBuffer{TEvent}.Next()"/> call.
+    /// </summary>
     public class InsufficientCapacityException : Exception
     {
         public static readonly InsufficientCapacityException Instance = new InsufficientCapacityException();
