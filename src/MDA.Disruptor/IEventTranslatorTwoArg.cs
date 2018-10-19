e377fc0bed8b861075bf60c20dad7d92c1518c9e
李二毛 <lijun@geekbuy.com.cn>
2018-10-19 18:11:16 +0800

    添加接口方法

--- src/MDA.Disruptor/IEventTranslatorTwoArg.cs
+++ src/MDA.Disruptor/IEventTranslatorTwoArg.cs
@@ -1,6 +1,22 @@
-﻿namespace MDA.Disruptor
+﻿using MDA.Disruptor.Impl;
+
+namespace MDA.Disruptor
 {
-    public interface IEventTranslatorTwoArg<TEvent, TArg0, TArg1>
+    /// <summary>
+    /// Implementations translate another data representations into events claimed from the <see cref="RingBuffer{TEvent}"/>.
+    /// </summary>
+    /// <typeparam name="TEvent">implementation storing the data for sharing during exchange or parallel coordination of an event.</typeparam>
+    /// <typeparam name="TArg0"></typeparam>
+    /// <typeparam name="TArg1"></typeparam>
+    public interface IEventTranslatorTwoArg<in TEvent, in TArg0, in TArg1>
     {
+        /// <summary>
+        /// Translate a data representation into fields set in given event.
+        /// </summary>
+        /// <param name="event">into which the data should be translated.</param>
+        /// <param name="sequence">that is assigned to event.</param>
+        /// <param name="arg0">The first user specified argument to the translator</param>
+        /// <param name="arg1">The second user specified argument to the translator</param>
+        void TranslateTo(TEvent @event, long sequence, TArg0 arg0, TArg1 arg1);
     }
 }
--- src/MDA.Disruptor/IEventTranslatorVarArg.cs
+++ src/MDA.Disruptor/IEventTranslatorVarArg.cs
@@ -1,6 +1,19 @@
-﻿namespace MDA.Disruptor
+﻿using MDA.Disruptor.Impl;
+
+namespace MDA.Disruptor
 {
-    public interface IEventTranslatorVarArg<TEvent>
+    /// <summary>
+    /// Implementations translate another data representations into events claimed from the <see cref="RingBuffer{TEvent}"/>.
+    /// </summary>
+    /// <typeparam name="TEvent">implementation storing the data for sharing during exchange or parallel coordination of an event.</typeparam>
+    public interface IEventTranslatorVarArg<in TEvent>
     {
+        /// <summary>
+        /// Translate a data representation into fields set in given event
+        /// </summary>
+        /// <param name="event">into which the data should be translated.</param>
+        /// <param name="sequence">that is assigned to event.</param>
+        /// <param name="args">The array of user arguments.</param>
+        void TranslateTo(TEvent @event, long sequence, params object[] args);
     }
 }
