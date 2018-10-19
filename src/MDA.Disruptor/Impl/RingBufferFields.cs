1291a9b2116618bb9d8ca97c18b6a3176d6d20db
李二毛 <lijun@geekbuy.com.cn>
2018-10-19 18:10:45 +0800

    添加RingBufferFields

---
+++ src/MDA.Disruptor/Impl/RingBufferFields.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using MDA.Disruptor.Extensions;
+
+namespace MDA.Disruptor.Impl
+{
+    public abstract class RingBufferPad
+    {
+        protected long P1, P2, P3, P4, P5, P6, P7;
+    }
+
+    public abstract class RingBufferFields<TEvent> : RingBufferPad
+    {
+        private static readonly int BufferPad = 128 / IntPtr.Size;
+
+        private readonly long _indexMask;
+        private readonly object[] _entries;
+
+        protected int BufferSize;
+        protected ISequencer Sequencer;
+
+        protected RingBufferFields(
+            ISequencer sequencer,
+            IEventFactory<TEvent> eventFactory)
+        {
+            Sequencer = sequencer;
+            BufferSize = Sequencer.BufferSize;
+
+            if (BufferSize < 1)
+            {
+                throw new ArgumentException("bufferSize must not be less than 1");
+            }
+
+            if (BufferSize.IsNotPowerOf2())
+            {
+                throw new ArgumentException("bufferSize must be a power of 2");
+            }
+
+            _indexMask = BufferSize - 1;
+            _entries = new object[sequencer.BufferSize + 2 * BufferPad];
+
+            Fill(eventFactory);
+        }
+
+        private void Fill(IEventFactory<TEvent> eventFactory)
+        {
+            for (int i = 0; i < BufferSize; i++)
+            {
+                _entries[BufferPad + i] = eventFactory.NewInstance();
+            }
+        }
+
+        protected TEvent ElementAt(long sequence)
+        {
+            return Utility.Util.Read<TEvent>(_entries, BufferPad + (int)(sequence & _indexMask));
+        }
+    }
+}
