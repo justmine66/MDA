83eb707216f3a3138c557d74cccfc584065bfec0
李二毛 <lijun@geekbuy.com.cn>
2018-10-20 16:26:40 +0800

    泛型化FatalExceptionHandler

--- src/MDA.Disruptor/Impl/FatalExceptionHandler.cs
+++ src/MDA.Disruptor/Impl/FatalExceptionHandler.cs
@@ -7,15 +7,15 @@ namespace MDA.Disruptor.Impl
     /// <summary>
     ///  Convenience implementation of an exception handler that using standard logging to log the exception as <see cref="LogLevel.Critical"/> and re-throw it wrapped in a <see cref="RuntimeException"/>
     /// </summary>
-    public class FatalExceptionHandler : IExceptionHandler<object>
+    public class FatalExceptionHandler<TEvent> : IExceptionHandler<TEvent>
     {
         private readonly ILogger _logger;
-        public FatalExceptionHandler(ILogger<FatalExceptionHandler> logger)
+        public FatalExceptionHandler(ILogger<FatalExceptionHandler<TEvent>> logger)
         {
             _logger = logger;
         }
 
-        public void HandleEventException(Exception ex, long sequence, object @event)
+        public void HandleEventException(Exception ex, long sequence, TEvent @event)
         {
             _logger.LogCritical("Exception processing: " + sequence + " " + @event, ex);
             throw new RuntimeException(ex);
