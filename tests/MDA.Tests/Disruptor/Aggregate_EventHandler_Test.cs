501d4b4e35810fc33284186ffcae93d8b6f122ec
李二毛 <lijun@geekbuy.com.cn>
2018-10-20 10:47:45 +0800

    添加测试方法

--- tests/MDA.Tests/Disruptor/Aggregate_EventHandler_Test.cs
+++ tests/MDA.Tests/Disruptor/Aggregate_EventHandler_Test.cs
@@ -21,12 +21,58 @@ namespace MDA.Tests.Disruptor
             AssertLastEvent(@event, sequence, _eh1, _eh2, _eh3);
         }
 
+        [Fact(DisplayName = "调用OnStart。")]
+        public void Should_Call_OnStart_InSequence()
+        {
+            var aggregateEventHandler = new AggregateEventHandler<int[]>(_eh1, _eh2, _eh3);
+
+            aggregateEventHandler.OnStart();
+
+            AssertStartCalls(1, _eh1, _eh2, _eh3);
+        }
+
+        [Fact(DisplayName = "调用OnShutdown。")]
+        public void Should_Call_OnShutdown_InSequence()
+        {
+            var aggregateEventHandler = new AggregateEventHandler<int[]>(_eh1, _eh2, _eh3);
+
+            aggregateEventHandler.OnShutdown();
+
+            AssertShutdownCalls(1, _eh1, _eh2, _eh3);
+        }
+
+        [Fact(DisplayName = "AggregateEventHandler应该能处理空EventHandlers调用。")]
+        public void Should_Handle_Empty_List_Of_EventHandlers()
+        {
+            var aggregateEventHandler = new AggregateEventHandler<int[]>();
+
+            aggregateEventHandler.OnEvent(new int[] { 7 }, 0L, true);
+            aggregateEventHandler.OnStart();
+            aggregateEventHandler.OnShutdown();
+        }
+
         private static void AssertLastEvent(int[] @event, long sequence, params DummyEventHandler<int[]>[] ehs)
         {
             foreach (var eh in ehs)
             {
                 Assert.Equal(@event, eh.LastEvent);
                 Assert.Equal(sequence, eh.LastSequence);
+            }
+        }
+
+        private static void AssertStartCalls(int startCalls, params DummyEventHandler<int[]>[] ehs)
+        {
+            foreach (var eh in ehs)
+            {
+                Assert.Equal(eh.StartCalls, startCalls);
+            }
+        }
+
+        private static void AssertShutdownCalls(int shutdownCalls, params DummyEventHandler<int[]>[] ehs)
+        {
+            foreach (var eh in ehs)
+            {
+                Assert.Equal(eh.ShutdownCalls, shutdownCalls);
             }
         }
     }
