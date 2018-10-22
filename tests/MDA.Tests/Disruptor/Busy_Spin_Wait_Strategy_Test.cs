d50371b4f358215356ed2dbe0f0e7c4461b53da8
justmine <justmine@DESKTOP-UILU6SC>
2018-10-22 05:59:34 -0700

    添加类文件

---
+++ tests/MDA.Tests/Disruptor/Busy_Spin_Wait_Strategy_Test.cs
@@ -0,0 +1,15 @@
+﻿namespace MDA.Tests.Disruptor
+{
+    using MDA.Disruptor.Impl;
+    using Xunit;
+    using static MDA.Tests.Disruptor.Support.WaitStrategyTestUtil;
+
+    public class Busy_Spin_Wait_Strategy_Test
+    {
+        [Fact(DisplayName = "应该等待")]
+        public void Should_Wait_For_Value()
+        {
+            AssertWaitForWithDelayOf(50, new BusySpinWaitStrategy());
+        }
+    }
+}
