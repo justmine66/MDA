54b67f67ae11ca55926375ad61fa7de69601667f
justmine <justmine@DESKTOP-UILU6SC>
2018-10-22 06:00:22 -0700

    实现方法AssertWaitForWithDelayOf

--- tests/MDA.Tests/Disruptor/Support/WaitStrategyTestUtil.cs
+++ tests/MDA.Tests/Disruptor/Support/WaitStrategyTestUtil.cs
@@ -1,11 +1,21 @@
 ﻿using MDA.Disruptor;
+using MDA.Disruptor.Impl;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace MDA.Tests.Disruptor.Support
 {
     public class WaitStrategyTestUtil
     {
-        public static void AssertWaitForWithDelayOf(long sleepTimeMillis, IWaitStrategy waitStrategy) {
+        public static void AssertWaitForWithDelayOf(int sleepTimeMillis, IWaitStrategy waitStrategy)
+        {
+            var sequenceUpdater = new SequenceUpdater(sleepTimeMillis, waitStrategy);
+            Task.Run(() => sequenceUpdater.Run());
+            sequenceUpdater.WaitForStartup();
+            var cursor = new Sequence(0);
+            var sequence = waitStrategy.WaitFor(0, cursor, sequenceUpdater.sequence, new DummySequenceBarrier());
 
+            Assert.Equal(0L, sequence);
         }
     }
 }
