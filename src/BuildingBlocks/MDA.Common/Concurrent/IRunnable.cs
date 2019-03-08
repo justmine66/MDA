bcd468b20f9838c3be733f884848929c4e6a92b8
justmine <lijun@geekbuy.com.cn>
2019-03-08 15:37:11 +0800

    重构

--- src/BuildingBlocks/MDA.Common/Concurrent/IRunnable.cs
+++ src/BuildingBlocks/MDA.Common/Concurrent/IRunnable.cs
@@ -1,12 +1,24 @@
-﻿namespace MDA.Common.Concurrent
+﻿using System.Threading.Tasks;
+
+namespace MDA.Common.Concurrent
 {
     public interface IRunnable
     {
         void Run();
     }
 
-    public interface IRunnable<in TState>
+    public interface IAsyncRunnable : IRunnable
+    {
+        Task RunAsync();
+    }
+
+    public interface IRunnable<in TState> where TState : class
     {
         void Run(TState state);
+    }
+
+    public interface IAsyncRunnable<in TState> : IRunnable<TState> where TState : class
+    {
+        Task RunAsync(TState state);
     }
 }
--- src/BuildingBlocks/MDA.Common/Concurrent/IScheduledExecutor.cs
+++ src/BuildingBlocks/MDA.Common/Concurrent/IScheduledExecutor.cs
@@ -3,18 +3,49 @@ using System.Threading.Tasks;
 
 namespace MDA.Common.Concurrent
 {
+    /// <summary>
+    /// Provides a mechanism for executing a method at specified intervals.
+    /// </summary>
     public interface IScheduledExecutor
     {
         /// <summary>
-        /// Executes the given command after the given delay.
+        /// Synchronously executes the given command after the given delay.
         /// </summary>
         /// <param name="command">the task to execute in the future.</param>
         /// <param name="delay">delay the time from now to delay the execution.</param>
-        /// <returns></returns>
-        Task ScheduleAsync(IRunnable command, TimeSpan delay);
+        /// <returns>This never completes unless the given command execution failed or cancelled.</returns>
+        ICancellable Schedule(IRunnable command, TimeSpan delay);
 
         /// <summary>
-        /// Executes the given command periodically.
+        /// Synchronously executes the given command after the given delay.
+        /// </summary>
+        /// <typeparam name="TState">the state type.</typeparam>
+        /// <param name="command">the task to execute in the future.</param>
+        /// <param name="delay">delay the time from now to delay the execution.</param>
+        /// <param name="state">An object containing information to be used by the command.</param>
+        /// <returns>This never completes unless the given command execution failed or cancelled.</returns>
+        ICancellable<TState> Schedule<TState>(IRunnable<TState> command, TimeSpan delay, TState state) where TState : class;
+
+        /// <summary>
+        /// Asynchronously executes the given command after the given delay.
+        /// </summary>
+        /// <param name="command">the task to execute in the future.</param>
+        /// <param name="delay">delay the time from now to delay the execution.</param>
+        /// <returns>This never completes unless the given command execution failed or cancelled.</returns>
+        Task<ICancellable> ScheduleAsync(IAsyncRunnable command, TimeSpan delay);
+
+        /// <summary>
+        /// Asynchronously executes the given command after the given delay.
+        /// </summary>
+        /// <typeparam name="TState">the state type.</typeparam>
+        /// <param name="command">the task to execute in the future.</param>
+        /// <param name="delay">delay the time from now to delay the execution.</param>
+        /// <param name="state">An object containing information to be used by the command.</param>
+        /// <returns>This never completes unless the given command execution failed or cancelled.</returns>
+        Task<ICancellable<TState>> ScheduleAsync<TState>(IAsyncRunnable<TState> command, TimeSpan delay, TState state) where TState : class;
+
+        /// <summary>
+        /// Synchronously executes the given command periodically.
         /// </summary>
         /// <remarks>
         /// The first execution is started after the <see cref="initialDelay"/>, the second execution is started after the <see cref="initialDelay"/> + <see cref="period"/>, the third execution is started after the <see cref="initialDelay"/> + 2*<see cref="period"/> and so on.
@@ -22,40 +53,87 @@ namespace MDA.Common.Concurrent
         /// <param name="command">the task to be executed periodically.</param>
         /// <param name="initialDelay">the time from now until the first execution is triggered.</param>
         /// <param name="period">the time after which the next execution is triggered.</param>
-        /// <returns>a ScheduledFuture representing the periodic task. This future never completes unless an execution of the given task fails or if the future is cancelled.</returns>
-        TaskCompletionSource<object> ScheduleAtFixedRateAsync(IRunnable command, TimeSpan initialDelay, TimeSpan period);
+        /// <returns>This never completes unless the given command execution failed or cancelled.</returns>
+        ICancellable ScheduleAtFixedRate(IRunnable command, TimeSpan initialDelay, TimeSpan period);
 
         /// <summary>
-        /// Executes the given command periodically.
+        /// Synchronously executes the given command periodically.
         /// </summary>
         /// <remarks>
         /// The first execution is started after the <see cref="initialDelay"/>, the second execution is started after the <see cref="initialDelay"/> + <see cref="period"/>, the third execution is started after the <see cref="initialDelay"/> + 2*<see cref="period"/> and so on.
         /// </remarks>
-        /// <typeparam name="TState">The state</typeparam>
+        /// <typeparam name="TState">the state type.</typeparam>
         /// <param name="command">the task to be executed periodically.</param>
         /// <param name="initialDelay">the time from now until the first execution is triggered.</param>
         /// <param name="period">the time after which the next execution is triggered.</param>
-        /// <returns>a ScheduledFuture representing the periodic task. This future never completes unless an execution of the given task fails or if the future is cancelled.</returns>
-        TaskCompletionSource<TState> ScheduleAtFixedRateAsync<TState>(IRunnable<TState> command, TimeSpan initialDelay, TimeSpan period);
+        /// <param name="state">An object containing information to be used by the command.</param>
+        /// <returns>This never completes unless the given command execution failed or cancelled.</returns>
+        ICancellable<TState> ScheduleAtFixedRate<TState>(IRunnable<TState> command, TimeSpan initialDelay, TimeSpan period, TState state) where TState : class;
 
         /// <summary>
-        /// Executed the given command repeatedly with the given delay between the end of an execution and the start of the next execution.
+        /// Asynchronously executes the given command periodically.
         /// </summary>
-        /// <typeparam name="T">The state</typeparam>
+        /// <remarks>
+        /// The first execution is started after the <see cref="initialDelay"/>, the second execution is started after the <see cref="initialDelay"/> + <see cref="period"/>, the third execution is started after the <see cref="initialDelay"/> + 2*<see cref="period"/> and so on.
+        /// </remarks>
+        /// <param name="command">the task to be executed periodically.</param>
+        /// <param name="initialDelay">the time from now until the first execution is triggered.</param>
+        /// <param name="period">the time after which the next execution is triggered.</param>
+        /// <returns>This never completes unless the given command execution failed or cancelled.</returns>
+        Task<ICancellable> ScheduleAtFixedRateAsync(IAsyncRunnable command, TimeSpan initialDelay, TimeSpan period);
+
+        /// <summary>
+        /// Asynchronously executes the given command periodically.
+        /// </summary>
+        /// <remarks>
+        /// The first execution is started after the <see cref="initialDelay"/>, the second execution is started after the <see cref="initialDelay"/> + <see cref="period"/>, the third execution is started after the <see cref="initialDelay"/> + 2*<see cref="period"/> and so on.
+        /// </remarks>
+        /// <typeparam name="TState">the state type.</typeparam>
+        /// <param name="command">the task to be executed periodically.</param>
+        /// <param name="initialDelay">the time from now until the first execution is triggered.</param>
+        /// <param name="period">the time after which the next execution is triggered.</param>
+        /// <param name="state">An object containing information to be used by the command.</param>
+        /// <returns>This never completes unless the given command execution failed or cancelled.</returns>
+        Task<ICancellable<TState>> ScheduleAtFixedRateAsync<TState>(IAsyncRunnable<TState> command, TimeSpan initialDelay, TimeSpan period, TState state) where TState : class;
+
+        /// <summary>
+        /// Synchronously executes the given command repeatedly with the given delay between the end of an execution and the start of the next execution.
+        /// </summary>
         /// <param name="command">the task to execute repeatedly.</param>
         /// <param name="initialDelay">the time from now until the first execution is triggered</param>
         /// <param name="delay">the time between the end of the current and the start of the next execution</param>
-        /// <returns>a ScheduledFuture representing the repeatedly executed task. This future never completes unless the execution of the given task fails or if the future is cancelled.</returns>
-        TaskCompletionSource<object> ScheduleWithFixedDelayAsync(IRunnable command, TimeSpan initialDelay, TimeSpan delay);
+        /// <returns>This never completes unless the given command execution failed or cancelled.</returns>
+        ICancellable ScheduleWithFixedDelay(IRunnable command, TimeSpan initialDelay, TimeSpan delay);
 
         /// <summary>
-        /// Executed the given command repeatedly with the given delay between the end of an execution and the start of the next execution.
+        /// Synchronously executes the given command repeatedly with the given delay between the end of an execution and the start of the next execution.
         /// </summary>
-        /// <typeparam name="TState">The state</typeparam>
+        /// <typeparam name="TState">the state type.</typeparam>
         /// <param name="command">the task to execute repeatedly.</param>
         /// <param name="initialDelay">the time from now until the first execution is triggered</param>
         /// <param name="delay">the time between the end of the current and the start of the next execution</param>
-        /// <returns>a ScheduledFuture representing the repeatedly executed task. This future never completes unless the execution of the given task fails or if the future is cancelled.</returns>
-        TaskCompletionSource<TState> ScheduleWithFixedDelayAsync<TState>(IRunnable<TState> command, TimeSpan initialDelay, TimeSpan delay);
+        /// <param name="state">An object containing information to be used by the command.</param>
+        /// <returns>This never completes unless the given command execution failed or cancelled.</returns>
+        ICancellable<TState> ScheduleWithFixedDelay<TState>(IRunnable<TState> command, TimeSpan initialDelay, TimeSpan delay, TState state) where TState : class;
+
+        /// <summary>
+        /// Asynchronously executes the given command repeatedly with the given delay between the end of an execution and the start of the next execution.
+        /// </summary>
+        /// <param name="command">the task to execute repeatedly.</param>
+        /// <param name="initialDelay">the time from now until the first execution is triggered</param>
+        /// <param name="delay">the time between the end of the current and the start of the next execution</param>
+        /// <returns>This never completes unless the given command execution failed or cancelled.</returns>
+        Task<ICancellable> ScheduleWithFixedDelayAsync(IAsyncRunnable command, TimeSpan initialDelay, TimeSpan delay);
+
+        /// <summary>
+        /// Asynchronously executes the given command repeatedly with the given delay between the end of an execution and the start of the next execution.
+        /// </summary>
+        /// <typeparam name="TState">the state type.</typeparam>
+        /// <param name="command">the task to execute repeatedly.</param>
+        /// <param name="initialDelay">the time from now until the first execution is triggered</param>
+        /// <param name="delay">the time between the end of the current and the start of the next execution</param>
+        /// <param name="state">An object containing information to be used by the command.</param>
+        /// <returns>This never completes unless the given command execution failed or cancelled.</returns>
+        Task<ICancellable<TState>> ScheduleWithFixedDelayAsync<TState>(IAsyncRunnable<TState> command, TimeSpan initialDelay, TimeSpan delay, TState state) where TState : class;
     }
 }
