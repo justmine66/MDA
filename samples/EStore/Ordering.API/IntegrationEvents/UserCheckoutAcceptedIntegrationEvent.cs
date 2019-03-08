567b15ad96ebbf8aaa065d4a163782317c8e0af5
justmine <lijun@geekbuy.com.cn>
2019-03-08 16:36:58 +0800

    删除多余项目

---
+++ samples/EStore/Ordering.API/IntegrationEvents/UserCheckoutAcceptedIntegrationEvent.cs
@@ -0,0 +1,13 @@
+﻿using MDA.MessageBus;
+
+namespace Ordering.API.IntegrationEvents
+{
+    /// <summary>
+    /// 下单事件
+    /// </summary>
+    public class UserCheckoutAcceptedIntegrationEvent : Message
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+    }
+}
