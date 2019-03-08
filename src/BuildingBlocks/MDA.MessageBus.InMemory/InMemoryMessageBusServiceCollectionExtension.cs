b5fb9aab418fbc70e4096f74a796b5b0884cefd9
justmine <lijun@geekbuy.com.cn>
2019-03-08 15:35:28 +0800

    添加内部版消息总线

---
+++ src/BuildingBlocks/MDA.MessageBus.InMemory/InMemoryMessageBusServiceCollectionExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MDA.MessageBus.InMemory
+{
+    public static class InMemoryMessageBusServiceCollectionExtension
+    {
+        public static IServiceCollection AddInMemoryMessageBusServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IMessageBus, InMemoryMessageBus>();
+            return services;
+        }
+    }
+}
