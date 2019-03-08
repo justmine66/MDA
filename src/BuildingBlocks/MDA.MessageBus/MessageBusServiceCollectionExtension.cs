b979acd3c7149ce3a4e92cd816f001f4840f4fe4
justmine <lijun@geekbuy.com.cn>
2019-03-08 14:27:40 +0800

    重构MessageBusServiceCollectionExtension

--- src/BuildingBlocks/MDA.MessageBus/MessageBusServiceCollectionExtension.cs
+++ src/BuildingBlocks/MDA.MessageBus/MessageBusServiceCollectionExtension.cs
@@ -1,15 +1,30 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace MDA.MessageBus
 {
     public static class MessageBusServiceCollectionExtension
     {
-        public static IServiceCollection AddMessageBusServices(this IServiceCollection services)
+        public static IServiceCollection AddMessageBusServices(this IServiceCollection services, Action<MessageOptions> options)
         {
-            services.AddSingleton<MessageOptions>();
+            return services
+                .AddMessageBusBasicServices()
+                .Configure(options);
+        }
+
+        public static IServiceCollection AddMessageBusServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services
+                .AddMessageBusBasicServices()
+                .Configure<MessageOptions>(configuration.GetSection(nameof(MessageOptions)));
+        }
+
+        private static IServiceCollection AddMessageBusBasicServices(this IServiceCollection services)
+        {
+            services.AddOptions();
             services.AddSingleton<IMessageSubscriberCollection, MessageSubscriberCollection>();
             services.AddSingleton<IMessageSubscriberManager, InMemoryMessageSubscriberManager>();
-
             return services;
         }
     }
