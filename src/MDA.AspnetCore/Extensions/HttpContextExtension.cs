6d745046bda9084b6958c9dc0deca20d5ab9c62e
justmine <lijun@geekbuy.com.cn>
2019-03-27 09:52:30 +0800

    添加项目MDA.AspnetCore

---
+++ src/MDA.AspnetCore/Extensions/HttpContextExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace MDA.AspnetCore.Extensions
+{
+    public static class HttpContextExtension
+    {
+        public static TServiceType GetService<TServiceType>(this HttpContext context) 
+            where TServiceType : class
+        {
+            return context.RequestServices.GetService(typeof(TServiceType)) as TServiceType;
+        }
+    }
+}
