3aaa5912f696560faa6b23ff43f9d5f2f1134fb2
justmine <lijun@geekbuy.com.cn>
2019-03-27 09:37:24 +0800

    第一次添加复制到从类

---
+++ src/MDA/Cluster/IReplicatable.cs
@@ -0,0 +1,12 @@
+﻿using MDA.Eventing;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MDA.Cluster
+{
+    public interface IReplicatable
+    {
+        Task<bool> ToSlave(InboundEvent @event);
+        Task<bool> ToSlave(IEnumerable<InboundEvent> events);
+    }
+}
