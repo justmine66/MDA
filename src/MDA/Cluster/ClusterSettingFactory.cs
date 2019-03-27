4ffe524fc732872c962b7ee4c973729a0167618f
justmine <lijun@geekbuy.com.cn>
2019-03-27 09:32:13 +0800

    添加类

---
+++ src/MDA/Cluster/ClusterSettingFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace MDA.Cluster
+{
+    public class ClusterSettingFactory
+    {
+        public static ClusterSetting Create()
+        {
+            return new ClusterSetting() { AppMode = new AppMode() { Environment = new Environment() } };
+        }
+    }
+}
