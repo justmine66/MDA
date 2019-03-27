ef7b682b718c94c052f52d56f38bd464e49f6d0f
justmine <lijun@geekbuy.com.cn>
2019-03-27 09:34:05 +0800

    添加文件

---
+++ src/MDA/Eventing/InboundEventReplicator.cs
@@ -0,0 +1,10 @@
+﻿namespace MDA.Eventing
+{
+    public class InboundEventReplicator<T> : IInBoundEventHandler<T> where T : InboundEvent, new()
+    {
+        public void OnEvent(T data, long sequence, bool endOfBatch)
+        {
+           
+        }
+    }
+}
