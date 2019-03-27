e8f8533a3b646cb09e1583a3cb5b5444a193a0ad
justmine <lijun@geekbuy.com.cn>
2019-03-27 14:08:45 +0800

    添加abstract

--- src/Application/MDA/Eventing/InboundEvent.cs
+++ src/Application/MDA/Eventing/InboundEvent.cs
@@ -5,7 +5,7 @@ namespace MDA.Eventing
     /// <summary>
     /// 表示一个从网线（事件总线等）上获取的入站事件。
     /// </summary>
-    public class InboundEvent : SequenceMessage
+    public abstract class InboundEvent : SequenceMessage
     {
 
     }
--- src/Application/MDA/Eventing/OutboundEvent.cs
+++ src/Application/MDA/Eventing/OutboundEvent.cs
@@ -5,7 +5,7 @@ namespace MDA.Eventing
     /// <summary>
     /// 表示一个出站事件，将被发送到网线（事件总线等）。
     /// </summary>
-    public class OutboundEvent : SequenceMessage
+    public abstract class OutboundEvent : SequenceMessage
     {
 
     }
