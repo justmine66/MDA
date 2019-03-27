a71eeeea739d57a29d4e1b2c732696d187060973
justmine <lijun@geekbuy.com.cn>
2019-03-27 14:06:17 +0800

    添加文件

---
+++ samples/EStore/Basket.API/Application/InboundEventHandlers/UpdateBasketInBoundEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using Basket.API.Port.Adapters.Inbound;
+using MDA.Eventing;
+
+namespace Basket.API.Application.InboundEventHandlers
+{
+    public class UpdateBasketInBoundEventHandler : IInBoundEventHandler<UpdateBasketInboundEvent>
+    {
+        public void OnEvent(UpdateBasketInboundEvent @event, long sequence, bool endOfBatch)
+        {
+           
+        }
+    }
+}
