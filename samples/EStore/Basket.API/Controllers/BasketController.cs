05db0428624197bb4fe11c3e7b57c70989a56856
justmine <lijun@geekbuy.com.cn>
2019-03-27 09:53:15 +0800

    重构IInboundDisruptor初始化逻辑

--- samples/EStore/Basket.API/Controllers/BasketController.cs
+++ samples/EStore/Basket.API/Controllers/BasketController.cs
@@ -1,8 +1,8 @@
 ﻿using Basket.API.Model;
 using Basket.API.Port.Adapters.Inbound;
+using MDA.AspnetCore.Extensions;
 using MDA.Concurrent;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 
 namespace Basket.API.Controllers
@@ -11,17 +11,11 @@ namespace Basket.API.Controllers
     [ApiController]
     public class BasketController : ControllerBase
     {
-        private readonly IInboundDisruptor<UpdateBasketInboundEvent> _disruptor;
-
-        public BasketController(ILogger<BasketController> logger, IInboundDisruptor<UpdateBasketInboundEvent> disruptor)
-        {
-            _disruptor = disruptor;
-        }
-
         [HttpPost]
         public async Task<ActionResult<CustomerBasket>> UpdateBasketAsync([FromBody] CustomerBasket basket, [FromHeader(Name = "x-requestid")] string requestId)
         {
-            var result = await _disruptor.PublishInboundEventAsync(new UpdateBasketEventTranslator(), requestId, basket);
+            var disruptor = HttpContext.GetService<IInboundDisruptor<UpdateBasketInboundEvent>>();
+            var result = await disruptor.PublishInboundEventAsync(new UpdateBasketEventTranslator(), requestId, basket);
             if (!result) return BadRequest();
 
             return Ok();
