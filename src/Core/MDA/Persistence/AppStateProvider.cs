82bb63d1c273bc69071daa2f604468a6aacb28d4
justmine <justmine@DESKTOP-UILU6SC>
2019-05-05 22:17:33 +0800

    添加缓存

--- src/Core/MDA/Persistence/AppStateProvider.cs
+++ src/Core/MDA/Persistence/AppStateProvider.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using MDA.Caching;
 using MDA.EventSourcing;
 using MDA.EventStoring;
 
@@ -9,24 +10,30 @@ namespace MDA.Persistence
     public class AppStateProvider : IAppStateProvider
     {
         private readonly IEventStore _eventStore;
-        private readonly 
+        private readonly IDataCache<object> _cache;
 
-        public AppStateProvider(IEventStore eventStore)
+        public AppStateProvider(IEventStore eventStore, IDataCache<object> cache)
         {
             _eventStore = eventStore;
+            _cache = cache;
         }
 
         public async Task<T> GetAsync<T>(string principal) where T : EventSourcedRootEntity
         {
+            var instance = _cache.Get(principal);
+            if (instance is T result)
+                return result;
+
             var eventLog = await _eventStore.GetEventLogAsync(principal);
 
             var constructor = typeof(T).GetConstructor(new[] { typeof(IEnumerable<IDomainEvent>), typeof(int) });
             if (constructor == null)
                 throw new InvalidOperationException($"A constructor for type '{typeof(T)}' was not found.");
 
-            var instance = constructor.Invoke(new object[] { eventLog, 1 });
+            var obj = constructor.Invoke(new object[] { eventLog, 1 }) as T;
+            _cache.TryAdd(principal, obj);
 
-            return instance as T;
+            return obj;
         }
     }
 }
