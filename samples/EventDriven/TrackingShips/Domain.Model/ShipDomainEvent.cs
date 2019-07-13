85715e37d0a269d8199b65479513794087330c27
justmine <lijun@geekbuy.com.cn>
2019-07-13 15:21:02 +0800

    1

---
+++ samples/EventDriven/TrackingShips/Domain.Model/ShipDomainEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MDA.EventSourcing;
+
+namespace TrackingShips.Domain.Model
+{
+    public class ShipDomainEvent : DomainEvent
+    {
+        public ShipDomainEvent()
+        {
+            Principal = new BusinessPrincipal()
+            {
+                Id = Guid.NewGuid().ToString(),
+                TypeName = typeof(Ship).FullName
+            };
+        }
+    }
+}
