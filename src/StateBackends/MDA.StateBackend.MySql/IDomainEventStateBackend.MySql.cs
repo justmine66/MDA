d4de8eb4ebd5b677b3b4dc300cc136ec941b4796
李春生 <lijun@geekbuy.com.cn>
2020-09-30 17:50:33 +0800

    第一次添加MySql状态后端

---
+++ src/StateBackends/MDA.StateBackend.MySql/IDomainEventStateBackend.MySql.cs
@@ -0,0 +1,40 @@
+﻿using MDA.Domain.Events;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MDA.StateBackend.MySql
+{
+    public class MySqlDomainEventStateBackend : IDomainEventStateBackend
+    {
+        private readonly MySqlStateBackendOptions _options;
+
+        public MySqlDomainEventStateBackend(IOptions<MySqlStateBackendOptions> options)
+        {
+            _options = options.Value;
+        }
+
+        public async Task AppendAsync(IDomainEvent @event, CancellationToken token = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task AppendAsync(IEnumerable<IDomainEvent> events, CancellationToken token = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<IDomainEvent>> GetEventStreamAsync(string aggregateRootId, long startOffset = 0, CancellationToken token = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<IDomainEvent>> GetEventStreamAsync(string aggregateRootId, long startOffset = 0, long endOffset = long.MaxValue,
+            CancellationToken token = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/StateBackends/MDA.StateBackend.MySql/MySqlStateBackendOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace MDA.StateBackend.MySql
+{
+    public class MySqlStateBackendOptions
+    {
+        public DomainEventOptions DomainEventOptions { get; set; }
+    }
+
+    public class DomainEventOptions
+    {
+        public string Table { get; set; } = "mda_domain_events";
+    }
+}
