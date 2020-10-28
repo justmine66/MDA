2917dac8c713601778a45075ce2230be3317a49c
李春生 <lijun@geekbuy.com.cn>
2020-10-28 17:59:09 +0800

    添加表达式对象映射器

---
+++ tests/MDA.XUnitTest/Shared/ObjectMapperTester.cs
@@ -0,0 +1,29 @@
+﻿using MDA.Shared.Mappers;
+using Xunit;
+
+namespace MDA.XUnitTest.Shared
+{
+    public class ObjectMapperTester
+    {
+        [Fact]
+        public void MapTest()
+        {
+            var people = new People() { Id = 1, Name = "a" };
+
+            var mapped = ObjectPortMapper<People, Cat>.Map(people);
+        }
+
+        public class People
+        {
+            public long Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class Cat
+        {
+            public long Id { get; set; }
+
+            public string Name { get; set; }
+        }
+    }
+}
