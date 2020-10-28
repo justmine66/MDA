61d18a6aa2cffb2930375d9f4e35d40528c174d9
李春生 <lijun@geekbuy.com.cn>
2020-10-28 17:59:54 +0800

    添加泛型缓存单元测试

---
+++ tests/MDA.XUnitTest/Shared/GenericCacheTester.cs
@@ -0,0 +1,26 @@
+﻿using MDA.Shared.DataStructures;
+using Xunit;
+
+namespace MDA.XUnitTest.Shared
+{
+    public class GenericCacheTester
+    {
+        [Fact]
+        public void CacheTest()
+        {
+            var people = new People() { Id = 1, Name = "a" };
+
+            GenericCache<People>.GetOrSet(() => people);
+
+            GenericCache<People>.Set(people);
+
+            GenericCache<People>.Get();
+        }
+
+        public class People
+        {
+            public long Id { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
