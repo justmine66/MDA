5d51e0de71572a650f86be1c3306980da792d022
justmine <3538307147@qq.com>
2020-11-13 14:23:30 +0800

    测试基于时间轮的调度器

---
+++ tests/MDA.XUnitTest/Shared/Serialization/BinarySerializerTester.cs
@@ -0,0 +1,54 @@
+﻿using MDA.Infrastructure.Serialization;
+using Xunit;
+
+namespace MDA.XUnitTest.Shared.Serialization
+{
+    public class BinarySerializerTester
+    {
+        [Fact]
+        public void TestBssom()
+        {
+            var serializer = new BssomBinarySerializer();
+
+            var source = new BankAccount();
+
+            var bytes = serializer.Serialize(source);
+
+            var sink = serializer.Deserialize<BankAccount>(bytes);
+
+            Assert.Equal(sink, source);
+        }
+    }
+
+    public interface IBankAccount
+    {
+
+    }
+
+    /// <summary>
+    /// 表示一个银行账户聚合根，封装状态。
+    /// </summary>
+    public class BankAccount : IBankAccount
+    {
+        /// <summary>
+        /// 账户名
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 余额
+        /// </summary>
+        public decimal Balance { get; set; }
+
+        /// <summary>
+        /// 开户行
+        /// </summary>
+        public string Bank { get; set; }
+
+        /// <summary>
+        /// 可用余额。
+        /// 计算规则：可用余额 = 账户余额 + 在途收入金额 - 在途支出金额。
+        /// </summary>
+        public decimal AvailableBalance { get; set; }
+    }
+}
---
+++ tests/MDA.XUnitTest/Shared/Serialization/JsonSerializerTester.cs
@@ -0,0 +1,22 @@
+﻿using MDA.Infrastructure.Serialization;
+using Xunit;
+
+namespace MDA.XUnitTest.Shared.Serialization
+{
+    public class JsonSerializerTester
+    {
+        [Fact]
+        public void TestBssom()
+        {
+            var serializer = new KoobooJsonSerializer();
+
+            IBankAccount source = new BankAccount();
+
+            var json = serializer.Serialize(source);
+
+            var sink = serializer.Deserialize<BankAccount>(json);
+
+            Assert.Equal(sink, source);
+        }
+    }
+}
