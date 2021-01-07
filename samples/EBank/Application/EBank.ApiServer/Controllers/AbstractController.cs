fbf59899ac0f3b289d4e897698eb77b5c3dce2ba
李春生 <lijun@geekbuy.com.cn>
2021-01-07 14:15:19 +0800

    feat(EBank.ApiServer)：继续添加控制器返回方法。

--- samples/EBank/Application/EBank.ApiServer/Controllers/AbstractController.cs
+++ samples/EBank/Application/EBank.ApiServer/Controllers/AbstractController.cs
@@ -23,13 +23,31 @@ namespace EBank.ApiServer.Controllers
         /// <summary>
         /// 创建一个 <see cref="OkObjectResult"/> 对象。
         /// </summary>
+        /// <param name="messages">消息</param>
+        /// <returns>结果</returns>
+        [NonAction]
+        public OkObjectResult Ok(params string[] messages) => Ok<object>(null, StatusCodes.Status200OK, messages);
+
+        /// <summary>
+        /// 创建一个 <see cref="OkObjectResult"/> 对象。
+        /// </summary>
+        /// <typeparam name="TPayload">内容载荷内心</typeparam>
+        /// <param name="payload">内容载荷</param>
+        /// <param name="messages">消息</param>
+        /// <returns>结果</returns>
+        [NonAction]
+        public OkObjectResult Ok<TPayload>(TPayload payload, params string[] messages) => Ok(payload, StatusCodes.Status200OK, messages);
+
+        /// <summary>
+        /// 创建一个 <see cref="OkObjectResult"/> 对象。
+        /// </summary>
         /// <typeparam name="TPayload">内容载荷内心</typeparam>
         /// <param name="payload">内容载荷</param>
         /// <param name="status">状态，通常为 HTTP 状态码，也可为业务约定代码。</param>
         /// <param name="messages">消息</param>
         /// <returns>结果</returns>
         [NonAction]
-        public OkObjectResult Ok<TPayload>(TPayload payload, int status = StatusCodes.Status202Accepted, params string[] messages)
+        public OkObjectResult Ok<TPayload>(TPayload payload, int status = StatusCodes.Status200OK, params string[] messages)
         {
             return base.Ok(ApiResult<TPayload>.Ok(payload, status, messages));
         }
@@ -41,6 +59,24 @@ namespace EBank.ApiServer.Controllers
         /// <returns>结果</returns>
         [NonAction]
         public override AcceptedResult Accepted(object value) => Accepted(value);
+
+        /// <summary>
+        /// 创建一个 <see cref="AcceptedResult"/> 对象。
+        /// </summary>
+        /// <param name="messages">消息</param>
+        /// <returns>结果</returns>
+        [NonAction]
+        public AcceptedResult Accepted(params string[] messages) => Accepted<object>(null, StatusCodes.Status202Accepted, messages);
+
+        /// <summary>
+        /// 创建一个 <see cref="AcceptedResult"/> 对象。
+        /// </summary>
+        /// <typeparam name="TPayload">内容载荷内心</typeparam>
+        /// <param name="payload">内容载荷</param>
+        /// <param name="messages">消息</param>
+        /// <returns>结果</returns>
+        [NonAction]
+        public AcceptedResult Accepted<TPayload>(TPayload payload, params string[] messages) => Accepted(payload, StatusCodes.Status200OK, messages);
 
         /// <summary>
         /// 创建一个 <see cref="AcceptedResult"/> 对象。
@@ -70,6 +106,14 @@ namespace EBank.ApiServer.Controllers
         /// <summary>
         /// 创建一个 <see cref="NotFoundObjectResult"/> 对象。
         /// </summary>
+        /// <param name="messages">消息</param>
+        /// <returns>结果</returns>
+        [NonAction]
+        public NotFoundObjectResult NotFound(params string[] messages) => NotFound(StatusCodes.Status202Accepted, messages);
+
+        /// <summary>
+        /// 创建一个 <see cref="NotFoundObjectResult"/> 对象。
+        /// </summary>
         /// <param name="status">状态，通常为 HTTP 状态码，也可为业务约定代码。</param>
         /// <param name="messages">消息</param>
         /// <returns>结果</returns>
@@ -89,6 +133,14 @@ namespace EBank.ApiServer.Controllers
         {
             return base.BadRequest(ApiErrorResult.BadRequest(value));
         }
+
+        /// <summary>
+        /// 创建一个 <see cref="BadRequestObjectResult"/> 对象。
+        /// </summary>
+        /// <param name="messages">消息</param>
+        /// <returns>结果</returns>
+        [NonAction]
+        public BadRequestObjectResult BadRequest(params string[] messages) => BadRequest(StatusCodes.Status202Accepted, messages);
 
         /// <summary>
         /// 创建一个 <see cref="BadRequestObjectResult"/> 对象。
