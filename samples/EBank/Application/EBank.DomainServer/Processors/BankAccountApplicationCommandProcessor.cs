126576c80e8275a7dd6f83f5e83259de70a03668
李春生 <lijun@geekbuy.com.cn>
2021-03-10 16:15:28 +0800

    refactor(EBank.Domain)：重新梳理领域分层。

--- samples/EBank/Application/EBank.DomainServer/Processors/BankAccountApplicationCommandProcessor.cs
+++ samples/EBank/Application/EBank.DomainServer/Processors/BankAccountApplicationCommandProcessor.cs
@@ -1,7 +1,6 @@
 ﻿using EBank.Application.Commanding.Accounts;
 using EBank.Domain.Commands.Accounts;
 using EBank.Domain.Models.Accounts;
-using EBank.Domain.Repositories.MySql;
 using MDA.Application.Commands;
 using System.Threading;
 using System.Threading.Tasks;
